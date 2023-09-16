06fd60a7c9f2cefe3ca16a4eee55a68b967d344c
Sergey Koshelev <shivacharya2019@gmail.com>
2023-09-16 16:49:35 +0300

    add storybook and typography Roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button as={'button'} variant={'primary'}>
+        button
+      </Button>
+      Hello
+      <Button as={'a'} variant={'primary'} href={'/link'}>
+        link
+      </Button>
+    </div>
+  )
 }
