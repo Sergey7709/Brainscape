ac0832bfeddfc0f8533750c1d343817d434b7551
Sergey Koshelev <shivacharya2019@gmail.com>
2023-09-17 14:30:51 +0300

    edit check-box.tsx

--- src/components/ui/check-box/check-box.tsx
+++ src/components/ui/check-box/check-box.tsx
@@ -1,10 +1,5 @@
-
 const CheckBox = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return <div>??????????</div>
+}
 
-export default CheckBox;
+export default CheckBox
