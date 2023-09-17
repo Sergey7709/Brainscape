cf6192fba4a3fc89ba99632429b454b92983eb0b
Mikhail Palkin <thefireofcoall@gmail.com>
2023-09-17 23:17:44 +0300

    typography + typography stories

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,31 +1,42 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import { clsx } from 'clsx'
+
 import s from './typography.module.scss'
 
-import {ElementType, ReactNode} from "react";
-import {clsx} from "clsx";
-
 type TypographyProps<T extends ElementType = 'span'> = {
-    as: T
-    variant?:
-        | 'large'
-        | 'h1'
-        | 'h2'
-        | 'h3'
-        | 'body1'
-        | 'body2'
-        | 'subtitle1'
-        | 'subtitle2'
-        | 'caption'
-        | 'overline'
-        | 'link1'
-        | 'link2'
-    children?: ReactNode
-    className?: string
-}
+  as?: T
+  variant?:
+    | 'large'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'body1'
+    | 'body2'
+    | 'subtitle1'
+    | 'subtitle2'
+    | 'caption'
+    | 'overline'
+    | 'link1'
+    | 'link2'
+  children?: ReactNode
+  className?: string
+} & ComponentPropsWithoutRef<T>
 
-export const Typography = ({as: Component = 'span', variant = 'body1', className, children}: TypographyProps) => {
+export const Typography = <T extends ElementType = 'span'>({
+  as,
+  variant = 'body1',
+  className,
+  children,
+  ...rest
+}: TypographyProps<T> & ComponentPropsWithoutRef<T>) => {
+  const Component = as ?? 'span'
 
-    // const classes = `${s[variant]} ${className}`
-    const classes = clsx(s[variant], className)
+  const classes = clsx(s[variant], className)
 
-    return <Component className={classes}>{children}</Component>
+  return (
+    <Component className={classes} {...rest}>
+      {children}
+    </Component>
+  )
 }
