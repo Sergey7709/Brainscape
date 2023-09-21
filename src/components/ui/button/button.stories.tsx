4d2543f0aa09a9bb0b8522fa227605e5fd871896
Sergey Koshelev <shivacharya2019@gmail.com>
2023-09-21 16:47:04 +0300

    edit button.stories.tsx and add render args in link for button storybook

--- src/components/ui/button/button.stories.tsx
+++ src/components/ui/button/button.stories.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button } from './button.tsx'
@@ -50,9 +52,39 @@ export const Link: Story = {
   args: {
     variant: 'link',
     children: 'Link-button',
-    disabled: false,
     as: 'a',
     href: 'https://webref.ru/',
+  },
+  render: args => {
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    return (
+      <>
+        <div style={{ marginBottom: '2rem' }}>
+          <input
+            type="radio"
+            id="enabled"
+            name="disableStatus"
+            checked={!isDisabled}
+            onChange={() => setIsDisabled(false)}
+          />
+          <label htmlFor="enabled" style={{ marginRight: '1rem' }}>
+            Enabled link
+          </label>
+          <input
+            type="radio"
+            id="disabled"
+            name="disableStatus"
+            checked={isDisabled}
+            onChange={() => setIsDisabled(true)}
+          />
+          <label htmlFor="disabled">Disabled link</label>
+        </div>
+        <div style={{ marginLeft: '3rem', cursor: 'not-allowed' }}>
+          <Button className={isDisabled ? 'disabled-link' : ''} {...args} />
+        </div>
+      </>
+    )
   },
 }
 
@@ -83,7 +115,6 @@ export const AsLink: Story = {
   args: {
     variant: 'primary',
     children: 'Link that looks like a button',
-    disabled: false,
     as: 'a',
     href: 'https://sass-lang.com',
   },
