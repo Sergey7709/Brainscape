a654ff525a312cc7f4cf94f469fb1eb1b50f39c3
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-01 23:08:24 +0300

    add Modal component for render in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Button } from '@/components/ui/button'
+import { Modal, ModalConstructor } from '@/components/ui/modal'
 
 export function App() {
   return (
-    <div>
+    <div
+      style={{
+        padding: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
       <Button as={'button'} variant={'primary'}>
         button primary
       </Button>
@@ -10,6 +20,22 @@ export function App() {
       <Button as={'a'} variant={'primary'} href={'/link'}>
         link
       </Button>
+      <Modal className={''} active>
+        <ModalConstructor.PortalAndOverlay>
+          <ModalConstructor.Head>Sign in</ModalConstructor.Head>
+          <ModalConstructor.Body>
+            <div className={'123'}></div>
+            <input placeholder="Username" />
+            <input placeholder="Password" type="password" />
+            <button>Sign in</button>
+          </ModalConstructor.Body>
+        </ModalConstructor.PortalAndOverlay>
+        <ModalConstructor.Trigger>
+          <Button as={'button'} variant={'tertiary'}>
+            Open the modal
+          </Button>
+        </ModalConstructor.Trigger>
+      </Modal>
     </div>
   )
 }
