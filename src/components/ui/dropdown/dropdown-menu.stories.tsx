eb695de4b8cc5eb6a8c79fd33b8586c2e9e4cbec
Mikhail Palkin <thefireofcoall@gmail.com>
2023-10-01 23:22:01 +0300

    raw dropdown-menu

--- src/components/ui/dropdown/dropdown-menu.stories.tsx
+++ src/components/ui/dropdown/dropdown-menu.stories.tsx
@@ -1,6 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { DropdownMenu } from '@/components/ui/dropdown/dropdown-menu.tsx'
+import { Eye } from '@/assets/icons'
+import testAva from '@/assets/test-avatar.jpg'
+import {
+  DropDownItem,
+  DropDownItemWithIcon,
+  DropdownMenu,
+} from '@/components/ui/dropdown/dropdown-menu.tsx'
+import { Typography } from '@/components/ui/typography'
 
 const meta = {
   title: 'Components/DropdownMenu',
@@ -11,6 +18,20 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: {},
+export const Default: Story = () => {
+  return (
+    <DropdownMenu trigger={<img src={testAva} alt={'trigger'} />}>
+      <DropDownItem>
+        <img src={testAva} alt={'test-avatar'} />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography>Ivan</Typography>
+          <Typography variant={'caption'} style={{ color: '#808080' }}>
+            email@gmail.com
+          </Typography>
+        </div>
+      </DropDownItem>
+      <DropDownItemWithIcon textValue={'Value two'} icon={<Eye />} />
+      <DropDownItemWithIcon textValue={'Value three'} icon={<Eye />} />
+    </DropdownMenu>
+  )
 }
--- src/components/ui/dropdown/dropdown-menu.tsx
+++ src/components/ui/dropdown/dropdown-menu.tsx
@@ -1,34 +1,109 @@
+import { CSSProperties, ReactNode } from 'react'
+
 import * as Dropdown from '@radix-ui/react-dropdown-menu'
+import { clsx } from 'clsx'
 
 import s from './dropdown-menu.module.scss'
 
-import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 
-export const DropdownMenu = () => {
+type DropdownMenuProps = {
+  align?: 'start' | 'center' | 'end'
+  trigger?: ReactNode
+  className?: string
+  style?: CSSProperties
+  children: ReactNode
+}
+
+export const DropdownMenu = ({
+  align = 'center',
+  trigger,
+  className,
+  style,
+  children,
+}: DropdownMenuProps) => {
   const classNames = {
-    content: s.content,
+    content: clsx(s.content, className),
     item: s.item,
     separator: s.separator,
+    arrowBox: s.arrowBox,
+    arrow: s.arrow,
+    itemsBox: s.itemsBox,
   }
 
   return (
     <Dropdown.Root>
-      <Dropdown.Trigger asChild>
-        <Button>Trigger</Button>
-      </Dropdown.Trigger>
+      <Dropdown.Trigger asChild>{trigger}</Dropdown.Trigger>
       <Dropdown.Portal>
-        <Dropdown.Content className={classNames.content} sideOffset={5}>
-          <Dropdown.Item className={classNames.item}>
-            <Typography>First item</Typography>
-          </Dropdown.Item>
-          <Dropdown.Separator className={classNames.separator} />
-          <Dropdown.Item className={classNames.item}>
-            <Typography>Second item</Typography>
-          </Dropdown.Item>
-          <Dropdown.Arrow />
+        <Dropdown.Content className={classNames.content} style={style} align={align} sideOffset={8}>
+          <Dropdown.Arrow className={classNames.arrowBox} asChild>
+            <div className={classNames.arrow} />
+          </Dropdown.Arrow>
+          <div className={classNames.itemsBox}>{children}</div>
         </Dropdown.Content>
       </Dropdown.Portal>
     </Dropdown.Root>
+  )
+}
+
+type DropDownItemProps = {
+  children: ReactNode
+  disabled?: boolean
+  onSelect?: (event: Event) => void
+  className?: string
+  style?: CSSProperties
+}
+
+export const DropDownItem = ({
+  children,
+  disabled,
+  onSelect,
+  className,
+  style,
+}: DropDownItemProps) => {
+  const classNames = {
+    item: clsx(s.item, className),
+  }
+
+  return (
+    <Dropdown.Item
+      className={classNames.item}
+      disabled={disabled}
+      onSelect={onSelect}
+      style={style}
+    >
+      {children}
+    </Dropdown.Item>
+  )
+}
+
+type DropDownItemWithIconProps = Omit<DropDownItemProps, 'children'> & {
+  icon: ReactNode
+  textValue: string
+}
+
+export const DropDownItemWithIcon = ({
+  icon,
+  textValue,
+  disabled,
+  onSelect,
+  className,
+  style,
+}: DropDownItemWithIconProps) => {
+  const classNames = {
+    item: clsx(s.item, className),
+    icon: s.itemIcon,
+  }
+
+  return (
+    <Dropdown.Item
+      className={classNames.item}
+      disabled={disabled}
+      onSelect={onSelect}
+      style={style}
+    >
+      <div className={s.itemIcon}>{icon}</div>
+      <Typography>{textValue}</Typography>
+    </Dropdown.Item>
   )
 }
