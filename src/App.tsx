595907fe28284b46e01f03f226fbc757c9c4c6f5
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-03 21:38:14 +0300

    Edit App JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { TextField } from './components/ui/textField'
+
 import { Button } from '@/components/ui/button'
 import { Modal, ModalConstructor } from '@/components/ui/modal'
+import { Typography } from '@/components/ui/typography'
 
 export function App() {
   return (
@@ -20,15 +23,32 @@ export function App() {
       <Button as={'a'} variant={'primary'} href={'/link'}>
         link
       </Button>
-      <Modal className={''} active>
+      <Modal showCloseButton={false} active>
         <ModalConstructor.PortalAndOverlay>
-          <ModalConstructor.Head>Sign in</ModalConstructor.Head>
+          <ModalConstructor.Head>
+            <Typography as={'span'} variant={'large'}>
+              Add New Card
+            </Typography>
+          </ModalConstructor.Head>
           <ModalConstructor.Body>
-            <div className={'123'}></div>
-            <input placeholder="Username" />
-            <input placeholder="Password" type="password" />
-            <button>Sign in</button>
+            <TextField label={'Choose a question format'}></TextField>
+            <TextField label={'Question'}></TextField>
+            <TextField label={'Answer'}></TextField>
           </ModalConstructor.Body>
+          <ModalConstructor.Footer>
+            <div style={{ width: '100px' }}>
+              <Button variant={'secondary'}>
+                <Typography as={'span'} variant={'body2'}>
+                  Cancel
+                </Typography>
+              </Button>
+            </div>
+            <Button variant={'primary'} fullWidth>
+              <Typography as={'span'} variant={'body2'}>
+                Add New Card
+              </Typography>
+            </Button>
+          </ModalConstructor.Footer>
         </ModalConstructor.PortalAndOverlay>
         <ModalConstructor.Trigger>
           <Button as={'button'} variant={'tertiary'}>
