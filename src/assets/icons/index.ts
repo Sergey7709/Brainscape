7138c3566d54316eb2672b5d53d14b4fb0d28f67
Andrei <zoitber9@mail.ru>
2023-10-06 20:29:14 +0300

    add pagination component and stories

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,6 +3,8 @@ export { default as Check } from './check'
 export { Logout } from './logout'
 export { Loading } from './loading'
 export { Close } from './close.tsx'
+export { ArrowLeft } from './arrow-left.tsx'
+export { ArrowRight } from './arrow-right.tsx'
 
 export { default as VisibilityOff } from './visibilityOff'
 export { default as Eye } from './eye'
