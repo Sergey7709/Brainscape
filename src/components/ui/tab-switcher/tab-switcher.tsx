fdf0a604927c7de8b96da2a07a48bc70ef3481b3
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-08 16:04:25 +0300

    Add tab-switcher.tsx

---
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -0,0 +1,84 @@
+import { FC, ReactNode } from 'react'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import { TabsContent, TabsListProps, TabsProps, TabsTrigger } from '@radix-ui/react-tabs'
+import { clsx } from 'clsx'
+
+import s from './tab-switcher.module.scss'
+
+export type TabsAsChildProps = { asChild?: boolean }
+
+export type TabDataProps = {
+  value: string
+  title: string
+  disabled?: boolean
+  content?: ReactNode | ReactNode[]
+}
+
+export type TabsTriggerProps = { fullWidth?: boolean; tabData: TabDataProps[] }
+
+export type TabsContentProps = { forceMount?: boolean }
+
+export type CombinedTabsProps = TabsAsChildProps &
+  TabsListProps &
+  TabsTriggerProps &
+  TabsContentProps &
+  TabsProps & { children?: ReactNode }
+
+export const TabSwitcher: FC<CombinedTabsProps> = props => {
+  const {
+    value,
+    children,
+    defaultValue,
+    onValueChange,
+    fullWidth = false,
+    tabData,
+    orientation = 'horizontal',
+    dir = 'ltr',
+    activationMode = 'automatic',
+    loop = true,
+    ...restProps
+  } = props
+
+  const renderTabsForTrigger = tabData?.map((el, index) => (
+    <TabsTrigger
+      key={el.value}
+      className={clsx(
+        s.TabsTrigger,
+        el.disabled && s.disabled,
+        fullWidth && s.fullWidth,
+        tabData.length - 1 === index && s.lastTab,
+        index === 0 && s.firstTab
+      )}
+      value={el.disabled ? 'disabled' : el.value}
+    >
+      {el.title}
+    </TabsTrigger>
+  ))
+
+  const renderChildrenOrTabContent =
+    children ||
+    tabData?.map(el => (
+      <TabsContent key={el.value} value={el.disabled ? '' : el.value}>
+        {el.content}
+      </TabsContent>
+    ))
+
+  return (
+    <Tabs.Root
+      className={s.TabsRoot}
+      defaultValue={defaultValue}
+      value={value}
+      onValueChange={onValueChange}
+      orientation={orientation}
+      activationMode={activationMode}
+      dir={dir}
+      asChild={restProps.asChild}
+    >
+      <Tabs.List className={s.TabsList} loop={loop}>
+        {renderTabsForTrigger}
+      </Tabs.List>
+      {renderChildrenOrTabContent}
+    </Tabs.Root>
+  )
+}
