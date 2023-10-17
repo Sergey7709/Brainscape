077376cfe418fa2024374755ddec8c114892ddfb
Mikhail Palkin <thefireofcoall@gmail.com>
2023-10-17 16:18:27 +0300

    inject endpoints, small refactoring

---
+++ src/service/index.ts
@@ -0,0 +1,4 @@
+export * from '@/service/cards/cards.service.ts'
+export * from '@/service/common/base-api.ts'
+export * from '@/service/decks/decks.service.ts'
+export * from '@/service/store.ts'
--- src/service/store.ts
+++ src/service/store.ts
@@ -1,15 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import { cardsApi } from '@/service/cars-api/cards-api.ts'
-import { decksApi } from '@/service/deck-api/decks-api.ts'
+import { baseApi } from '@/service/common/base-api.ts'
 
 export const store = configureStore({
   reducer: {
-    [decksApi.reducerPath]: decksApi.reducer,
-    [cardsApi.reducerPath]: cardsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(decksApi.middleware).concat(cardsApi.middleware),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 export type AppDispatch = typeof store.dispatch
