8477670775e082a6a56abc56f8a4367397831ad1
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-20 15:28:55 +0300

    Add loader

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
 import { SignIn } from '@/components/auth/sign-in'
+import { Loader } from '@/components/ui/loader/loader.tsx'
 import { useGetAuthUserMeDataQuery, useSignInUserMutation } from '@/service'
 
 export const Login = () => {
@@ -10,7 +11,7 @@ export const Login = () => {
 
   const isAuthenticated = !isError
 
-  if (isLoading) return <div>Loading...</div> ///!!!!!!!!!!!!
+  if (isLoading) return <Loader />
 
   if (isAuthenticated) return <Navigate to={'/'} replace={true} />
 
