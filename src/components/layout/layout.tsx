0b8bc926fad0410fbfea0aee24d285f6a7469e99
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-22 17:17:46 +0300

    Add getLogOut function for Header

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,15 +3,18 @@ import { Outlet } from 'react-router-dom'
 import s from './layout.module.scss'
 
 import { Header } from '@/components/header'
+import { useLogoutUserMutation } from '@/service'
 
 export const Layout = () => {
   const classNames = {
     container: s.container,
   }
 
+  const [getLogOut] = useLogoutUserMutation()
+
   return (
     <div className={classNames.container}>
-      <Header isAuth={true} user={{ name: 'ivan', email: 'test' }} />
+      <Header isAuth={true} user={{ name: 'ivan', email: 'test' }} onSignOut={getLogOut} />
       <Outlet />
     </div>
   )
