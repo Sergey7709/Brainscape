1cc627014bead4e700bed38f2ae05bbcc45703d2
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-23 11:32:59 +0300

    Add useGetAuthUserMeDataQuery, useLogoutUserMutation, userData. Edit props for children component Header

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,18 +3,26 @@ import { Outlet } from 'react-router-dom'
 import s from './layout.module.scss'
 
 import { Header } from '@/components/header'
-import { useLogoutUserMutation } from '@/service'
+import { Loader } from '@/components/ui/loader/loader.tsx'
+import { useGetAuthUserMeDataQuery, useLogoutUserMutation } from '@/service'
 
 export const Layout = () => {
   const classNames = {
     container: s.container,
   }
+  const { isSuccess: isAuthenticated, isLoading, data } = useGetAuthUserMeDataQuery()
 
   const [getLogOut] = useLogoutUserMutation()
 
+  const userData = !!data && {
+    name: data.name,
+    email: data.email,
+  }
+
   return (
     <div className={classNames.container}>
-      <Header isAuth={true} user={{ name: 'ivan', email: 'test' }} onSignOut={getLogOut} />
+      {isLoading && <Loader />}
+      <Header isAuth={isAuthenticated} user={userData || undefined} onSignOut={getLogOut} />
       <Outlet />
     </div>
   )
