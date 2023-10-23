e360791a2cefe546cf62a26a543aaeba86bab0a3
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-23 12:58:14 +0300

    Add SCSS registration.tsx

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -1,8 +1,14 @@
+import s from './registration.module.scss'
+
 import { SignUp } from '@/components/auth/sign-up'
 import { useSignUpUserMutation } from '@/service'
 
 export const Registration = () => {
   const [getSignUp] = useSignUpUserMutation()
 
-  return <SignUp onSubmitHandler={getSignUp} />
+  return (
+    <div className={s.signUp}>
+      <SignUp onSubmitHandler={getSignUp} />
+    </div>
+  )
 }
