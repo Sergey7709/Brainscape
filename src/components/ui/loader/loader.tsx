1c3f547dc22e808bcc57213dba6e6694962ea565
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-23 12:55:44 +0300

    Edit loader.tsx

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -1,22 +1,9 @@
-import { FC } from 'react'
-
 import s from './loader.module.scss'
 
-type LoaderProps = {
-  text?: string
-}
-
-export const Loader: FC<LoaderProps> = ({ text = 'LOADING....' }) => {
+export const Loader = () => {
   return (
     <div className={s.overlay}>
-      <div className={s.container}>
-        {/*<div className={s.folder}>*/}
-        {/*  <div className={s.top}></div>*/}
-        {/*  <div className={s.bottom}></div>*/}
-        {/*</div>*/}
-        <div className={s.title}>{text}</div>
-        <div className={s.loader}></div>
-      </div>
+      <div className={s.loader}></div>
     </div>
   )
 }
