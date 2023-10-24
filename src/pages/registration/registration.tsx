853b31d4ad13c03c3976b8b43cc34d75c4a5367f
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-24 19:46:39 +0300

    Add handleSubmitSignUp

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -1,14 +1,29 @@
+import { useNavigate } from 'react-router-dom'
+
 import s from './registration.module.scss'
 
 import { SignUp } from '@/components/auth/sign-up'
 import { useSignUpUserMutation } from '@/service'
+import { SignUpRequest } from '@/service/auth/auth.types.ts'
 
 export const Registration = () => {
   const [getSignUp] = useSignUpUserMutation()
 
+  const navigate = useNavigate()
+  const handleSubmitSignUp = async (data: SignUpRequest) => {
+    await getSignUp(data).unwrap()
+
+    try {
+      navigate('/')
+    } catch (error: any) {
+      ///!!!!!!!!!!!!!! уточнить тип
+      console.log(error.data.message)
+    } ///!!!!!!!!!!!!!! заменить лог
+  }
+
   return (
     <div className={s.signUp}>
-      <SignUp onSubmitHandler={getSignUp} />
+      <SignUp onSubmitHandler={handleSubmitSignUp} />
     </div>
   )
 }
