26161ac7b4b91ec04cdebbc203fc26a8d74a33d7
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-28 15:47:20 +0300

    Add memo and the logic has been changed and the EditableField component has been separated from the component

--- src/components/ui/editable-span/editable-span.tsx
+++ src/components/ui/editable-span/editable-span.tsx
@@ -1,77 +1,31 @@
-import { ChangeEvent, KeyboardEvent, useState } from 'react'
+import { memo } from 'react'
 
-import s from './editable-span.module.scss'
-
-import { Logout, Redactor } from '@/assets/icons'
 import { Button } from '@/components/ui/button'
-import { TextField } from '@/components/ui/textField'
-import { Typography } from '@/components/ui/typography'
+import s from '@/components/ui/editable-span/editable-span.module.scss'
+import { EditableField } from '@/components/ui/editable-span/EditableField.tsx'
 
 type EditableSpanProps = {
   name: string
   email: string
   handleLogout: () => void
-  onValueChange: (newName: string) => void
-  onCancel?: () => void
+  onNameChange: (newName: string) => void
+  onEmailChange: (newEmail: string) => void
 }
 
-export const EditableSpan = ({ name, email, handleLogout, onValueChange }: EditableSpanProps) => {
-  const [editMode, setEditMode] = useState(false)
-  const [title, setTitle] = useState(name)
+export const EditableSpan = memo(
+  ({ name, email, handleLogout, onNameChange, onEmailChange }: EditableSpanProps) => {
+    console.log('span')
 
-  const activateEditMode = () => {
-    setEditMode(true)
-    setTitle(name)
+    return (
+      <>
+        <EditableField initialValue={name} label="Nickname" onValueChange={onNameChange} />
+        <EditableField initialValue={email} label="Email" onValueChange={onEmailChange} />
+        <div className={s.logoutButton}>
+          <Button variant="secondary" onClick={handleLogout} fullWidth>
+            Logout
+          </Button>
+        </div>
+      </>
+    )
   }
-
-  const activateViewMode = () => {
-    setEditMode(false)
-    onValueChange(title)
-  }
-  const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value)
-  }
-
-  const handleKeyPress = (event: KeyboardEvent<HTMLSpanElement>) => {
-    if (event.key === 'Enter') {
-      event.currentTarget.blur()
-    }
-  }
-
-  return editMode ? (
-    <>
-      <TextField
-        className={s.nameInput}
-        type={'text'}
-        value={title}
-        onChange={changeTitle}
-        onBlur={activateViewMode}
-        onKeyDown={handleKeyPress}
-        label={'Nickname'}
-      />
-      <Button variant={'primary'} fullWidth={true} onClick={activateViewMode}>
-        Save Changes
-      </Button>
-    </>
-  ) : (
-    <>
-      <div className={s.nameContainer}>
-        <Typography variant={'h1'} onDoubleClick={activateEditMode}>
-          {name}
-        </Typography>
-        <Button variant={'link'} className={s.editNameButton} onClick={activateEditMode}>
-          <Redactor onClick={activateEditMode} />
-        </Button>
-      </div>
-      <Typography variant="body2" className={s.email}>
-        {email}
-      </Typography>
-      <div className={s.buttonContainer}>
-        <Button variant={'secondary'} onClick={handleLogout}>
-          <Logout />
-          Logout
-        </Button>
-      </div>
-    </>
-  )
-}
+)
