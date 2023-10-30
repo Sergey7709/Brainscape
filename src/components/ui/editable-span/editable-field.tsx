81de00ddc3c7ca34ae95ddf3b2b4a954ca4b8b96
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-30 17:17:19 +0300

    Delete comments

--- src/components/ui/editable-span/editable-field.tsx
+++ src/components/ui/editable-span/editable-field.tsx
@@ -1,16 +1,7 @@
 import { ChangeEvent, KeyboardEvent, memo, useState } from 'react'
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-
-import { Redactor } from '@/assets/icons'
-import { Button } from '@/components/ui/button'
 import { EditModeProfile } from '@/components/ui/editable-span/edit-mode-profile.tsx'
-import s from '@/components/ui/editable-span/editable-span.module.scss'
 import { ViewModeProfile } from '@/components/ui/editable-span/view-mode-profile.tsx'
-import { TextField } from '@/components/ui/textField'
-import { Typography } from '@/components/ui/typography'
 
 export type EditableFieldProps = {
   initialValue: string
@@ -18,39 +9,9 @@ export type EditableFieldProps = {
   onValueChange: (newValue: string) => void
 }
 
-// const getValidationSchema = (label: string) => {
-//   switch (label) {
-//     case 'Email':
-//       return z.object({
-//         Email: z.string().trim().email(),
-//       })
-//     case 'Nickname':
-//       return z.object({
-//         Nickname: z.string().trim().min(3).max(30),
-//       })
-//     default:
-//       return z.object({})
-//   }
-// }
-
 export const EditableField = memo(({ initialValue, label, onValueChange }: EditableFieldProps) => {
   const [editMode, setEditMode] = useState(false)
   const [value, setValue] = useState(initialValue)
-
-  // const validationSchema = getValidationSchema(label)
-
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm({
-  //   mode: 'onBlur',
-  //   reValidateMode: 'onBlur',
-  //   defaultValues: { Nickname: initialValue, Email: initialValue },
-  //   resolver: zodResolver(validationSchema),
-  // })
-  //
-  // const textField = register(label)
 
   const activateEditMode = () => {
     setEditMode(true)
@@ -86,41 +47,8 @@ export const EditableField = memo(({ initialValue, label, onValueChange }: Edita
         handleKeyPress={handleKeyPress}
         handleSaveChanges={handleSaveChanges}
       />
-      {/*<TextField*/}
-      {/*  type="text"*/}
-      {/*  value={value}*/}
-      {/*  {...textField}*/}
-      {/*  onChange={e => {*/}
-      {/*    textField.onChange(e)*/}
-      {/*    handleChange(e)*/}
-      {/*  }}*/}
-      {/*  errorMessage={errors.Email?.message || errors.Nickname?.message}*/}
-      {/*  onBlur={handleSubmit(handleSaveChanges)}*/}
-      {/*  onKeyDown={handleKeyPress}*/}
-      {/*  label={label}*/}
-      {/*/>*/}
-      {/*<Button*/}
-      {/*  variant="primary"*/}
-      {/*  fullWidth={true}*/}
-      {/*  className={s.buttonSave}*/}
-      {/*  onClick={handleSubmit(handleSaveChanges)}*/}
-      {/*>*/}
-      {/*  Save Changes*/}
-      {/*</Button>*/}
     </>
   ) : (
-    // <div className={s.nameContainer}>
-    //   <Typography
-    //     variant="h1"
-    //     className={label === 'Nickname' ? s.name : s.email}
-    //     onDoubleClick={handleSaveChanges}
-    //   >
-    //     {initialValue}
-    //   </Typography>
-    //   <Button variant="link" className={s.editNameButton} onClick={activateEditMode}>
-    //     <Redactor />
-    //   </Button>
-    // </div>
     <>
       <ViewModeProfile
         label={label}
