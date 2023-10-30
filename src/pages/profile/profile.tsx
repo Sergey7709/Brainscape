05a380328b74ca27e5603b1934070be0be76fd5f
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-30 17:25:09 +0300

    Delete comments and import

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,7 +1,6 @@
 import { memo, useCallback } from 'react'
 
 import { NavLink } from 'react-router-dom'
-import { toast } from 'react-toastify'
 
 import s from './profile.module.scss'
 
@@ -9,34 +8,11 @@ import { PersonalInformation } from '@/components/profile/personal-information'
 import { Button } from '@/components/ui/button'
 import { Loader } from '@/components/ui/loader'
 import { useUtilityFormData } from '@/pages/profile/useUtilityFormData.ts'
-import {
-  useGetAuthUserMeDataQuery,
-  useLogoutUserMutation,
-  useUpdateAuthUserDataMutation,
-} from '@/service'
+import { useGetAuthUserMeDataQuery, useLogoutUserMutation } from '@/service'
 
 export const Profile = memo(() => {
   const { data } = useGetAuthUserMeDataQuery()
   const [logOut] = useLogoutUserMutation()
-  // const [updateUserProfile, isLoading] = useUpdateAuthUserDataMutation()
-  //
-  // const utilityFormData = async <T extends File | string>(key: string, dataForm: T) => {
-  //   if (dataForm !== null) {
-  //     const dataRequest = new FormData()
-  //
-  //     dataRequest.append(key, dataForm)
-  //
-  //     await updateUserProfile(dataRequest)
-  //       .unwrap()
-  //       .catch(error => {
-  //         if (error.status === 400) {
-  //           toast.error(error.data.errorMessages[0].message)
-  //         } else if (error.status === 500) {
-  //           toast.error(error.data.message)
-  //         }
-  //       })
-  //   }
-  // }
 
   const { utilityFormData, isLoading } = useUtilityFormData()
 
