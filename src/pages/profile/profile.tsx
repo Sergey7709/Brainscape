fb4e391451ab2083a42f080573bdfa24130fd614
Sergey Koshelev <shivacharya2019@gmail.com>
2023-10-30 17:19:53 +0300

    function utilityFormData and useUpdateAuthUserDataMutation hook, put into a custom hook useUtilityFormData. Add Button for navigate to={'/'}

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,11 +1,14 @@
 import { memo, useCallback } from 'react'
 
+import { NavLink } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import s from './profile.module.scss'
 
 import { PersonalInformation } from '@/components/profile/personal-information'
+import { Button } from '@/components/ui/button'
 import { Loader } from '@/components/ui/loader'
+import { useUtilityFormData } from '@/pages/profile/useUtilityFormData.ts'
 import {
   useGetAuthUserMeDataQuery,
   useLogoutUserMutation,
@@ -15,25 +18,27 @@ import {
 export const Profile = memo(() => {
   const { data } = useGetAuthUserMeDataQuery()
   const [logOut] = useLogoutUserMutation()
-  const [updateUserProfile, isLoading] = useUpdateAuthUserDataMutation()
+  // const [updateUserProfile, isLoading] = useUpdateAuthUserDataMutation()
+  //
+  // const utilityFormData = async <T extends File | string>(key: string, dataForm: T) => {
+  //   if (dataForm !== null) {
+  //     const dataRequest = new FormData()
+  //
+  //     dataRequest.append(key, dataForm)
+  //
+  //     await updateUserProfile(dataRequest)
+  //       .unwrap()
+  //       .catch(error => {
+  //         if (error.status === 400) {
+  //           toast.error(error.data.errorMessages[0].message)
+  //         } else if (error.status === 500) {
+  //           toast.error(error.data.message)
+  //         }
+  //       })
+  //   }
+  // }
 
-  const utilityFormData = async <T extends File | string>(key: string, dataForm: T) => {
-    if (dataForm !== null) {
-      const dataRequest = new FormData()
-
-      dataRequest.append(key, dataForm)
-
-      await updateUserProfile(dataRequest)
-        .unwrap()
-        .catch(error => {
-          if (error.status === 400) {
-            toast.error(error.data.errorMessages[0].message)
-          } else if (error.status === 500) {
-            toast.error(error.data.message)
-          }
-        })
-    }
-  }
+  const { utilityFormData, isLoading } = useUtilityFormData()
 
   const handlerOnAvatarChange = useCallback(
     (newAvatar: File) => {
@@ -69,6 +74,11 @@ export const Profile = memo(() => {
           onEmailChange={handlerOnEmailChange}
         />
       )}
+      <div className={s.button}>
+        <Button variant={'tertiary'} as={NavLink} to={'/'} className={s.btn} fullWidth>
+          Back to main page
+        </Button>
+      </div>
     </div>
   )
 })
