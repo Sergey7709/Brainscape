14cdba402f51b89f24eb8a96238126635034faf4
Mikhail Palkin <thefireofcoall@gmail.com>
2023-10-31 19:23:32 +0300

    small updates

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,24 +1,21 @@
 import s from './decks.module.scss'
 
-import { Delete, Play, Redactor } from '@/assets/icons'
 import { Button } from '@/components/ui/button'
-import { Slider } from '@/components/ui/slider'
-import { TabSwitcher } from '@/components/ui/tab-switcher'
 import { Table } from '@/components/ui/tables/tables'
-import { TextField } from '@/components/ui/textField'
 import { Typography } from '@/components/ui/typography'
+import { DeckRow } from '@/pages/decks/deck-row/deck-row.tsx'
+import { DecksPanel } from '@/pages/decks/decks-panel/decks-panel.tsx'
 import { useGetDecksQuery } from '@/service'
 
 export const Decks = () => {
   const { data, isSuccess } = useGetDecksQuery()
 
   const classNames = {
+    container: s.container,
     decksPanel: s.decksPanel,
     searchInput: s.searchInput,
-    container: s.container,
     slider: s.slider,
     tableWrapper: s.tableWrapper,
-    buttonFilling: s.buttonFilling,
     head: s.head,
   }
 
@@ -28,29 +25,7 @@ export const Decks = () => {
         <Typography variant={'large'}>Packs list</Typography>
         <Button>Add new pack</Button>
       </div>
-      <div className={classNames.decksPanel}>
-        <div className={classNames.searchInput}>
-          <TextField type={'search'} placeholder={'Input search'} />
-        </div>
-        <TabSwitcher
-          label={'Show packs cards'}
-          defaultValue={'allCards'}
-          tabData={[
-            {
-              title: 'My cards',
-              value: 'myCards',
-            },
-            {
-              title: 'All cards',
-              value: 'allCards',
-            },
-          ]}
-        />
-        <Slider className={classNames.slider} min={0} max={10} label={'Number of cards'} />
-        <Button variant={'secondary'}>
-          <Delete /> Clear Filter
-        </Button>
-      </div>
+      <DecksPanel />
       <div className={classNames.tableWrapper}>
         <Table.Root>
           <Table.Head>
@@ -63,39 +38,7 @@ export const Decks = () => {
             </Table.Row>
           </Table.Head>
           <Table.Body>
-            {isSuccess &&
-              data.items.map(deck => {
-                const updatedDateFormat = new Date(deck.updated)
-                  .toISOString()
-                  .slice(0, 10)
-                  .split('-')
-                  .reverse()
-                  .join('.')
-
-                return (
-                  <Table.Row key={deck.id}>
-                    <Table.Cell>{deck.name}</Table.Cell>
-                    <Table.Cell>{deck.cardsCount}</Table.Cell>
-                    <Table.Cell>{updatedDateFormat}</Table.Cell>
-                    <Table.Cell>
-                      <Typography>{deck.author.name}</Typography>
-                    </Table.Cell>
-                    <Table.Cell>
-                      <div className={s.buttonContainer}>
-                        <Button variant="link" className={s.editAvatarButton}>
-                          <Play />
-                        </Button>
-                        <Button variant="link" className={s.editAvatarButton}>
-                          <Redactor />
-                        </Button>
-                        <Button variant="link" className={s.editAvatarButton}>
-                          <Delete />
-                        </Button>
-                      </div>
-                    </Table.Cell>
-                  </Table.Row>
-                )
-              })}
+            {isSuccess && data.items.map(deck => <DeckRow key={deck.id} {...deck} />)}
           </Table.Body>
         </Table.Root>
       </div>
