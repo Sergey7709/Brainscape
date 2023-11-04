702d9efef4610b68f31b211c80478a0ca1d1bc51
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-04 11:55:21 +0300

    useGetDecksQuery, useState<Sort>, sortedData are moved to the hook useGetDataSort

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,14 +1,41 @@
+import { useMemo, useState } from 'react'
+
 import s from './decks.module.scss'
 
 import { Button } from '@/components/ui/button'
-import { Table } from '@/components/ui/tables/tables'
+import { Sort, Table } from '@/components/ui/tables/tables'
 import { Typography } from '@/components/ui/typography'
+import { columns } from '@/pages/decks/constantsDeck.ts'
 import { DeckRow } from '@/pages/decks/deck-row/deck-row.tsx'
 import { DecksPanel } from '@/pages/decks/decks-panel/decks-panel.tsx'
+import { useGetDataSort } from '@/pages/decks/useGetDataSort.ts'
 import { useGetDecksQuery } from '@/service'
 
 export const Decks = () => {
-  const { data, isSuccess } = useGetDecksQuery()
+  // const { data, isSuccess } = useGetDecksQuery()
+  // const [sort, setSort] = useState<Sort>(null)
+  // const sortString: string | null = sort ? `${sort?.key}-${sort?.direction}` : null
+  //
+  // const sortedData = useMemo(() => {
+  //   if (isSuccess && !!data.items) {
+  //     if (!sortString) {
+  //       return data.items
+  //     }
+  //     const [key, direction] = sortString.split('-')
+  //
+  //     return [...data.items].sort((a, b) => {
+  //       if (direction === 'asc') {
+  //         return a[key as keyof typeof a] > b[key as keyof typeof b] ? 1 : -1
+  //       }
+  //
+  //       return a[key as keyof typeof a] < b[key as keyof typeof b] ? 1 : -1
+  //     })
+  //   } else {
+  //     return []
+  //   } ///!!!
+  // }, [sortString, data, isSuccess])
+
+  const { sort, setSort, sortedData, isSuccess } = useGetDataSort()
 
   const classNames = {
     container: s.container,
@@ -18,6 +45,8 @@ export const Decks = () => {
     tableWrapper: s.tableWrapper,
     head: s.head,
   }
+
+  console.log('sortedData', sortedData)
 
   return (
     <div className={classNames.container}>
@@ -29,17 +58,19 @@ export const Decks = () => {
         <DecksPanel />
         <div className={classNames.tableWrapper}>
           <Table.Root>
-            <Table.Head>
-              <Table.Row>
-                <Table.HeadCell>Name</Table.HeadCell>
-                <Table.HeadCell>Cards</Table.HeadCell>
-                <Table.HeadCell>Last Updated</Table.HeadCell>
-                <Table.HeadCell>Created by</Table.HeadCell>
-                <Table.HeadCell></Table.HeadCell>
-              </Table.Row>
-            </Table.Head>
+            <Table.Header columns={columns} sort={sort} onSort={setSort} />
+            {/*<Table.Head>*/}
+            {/*  <Table.Row>*/}
+            {/*    <Table.HeadCell>Name</Table.HeadCell>*/}
+            {/*    <Table.HeadCell>Cards</Table.HeadCell>*/}
+            {/*    <Table.HeadCell>Last Updated</Table.HeadCell>*/}
+            {/*    <Table.HeadCell>Created by</Table.HeadCell>*/}
+            {/*    <Table.HeadCell></Table.HeadCell>*/}
+            {/*  </Table.Row>*/}
+            {/*</Table.Head>*/}
             <Table.Body>
-              {isSuccess && data.items.map(deck => <DeckRow key={deck.id} {...deck} />)}
+              {/*{isSuccess && data.items.map(deck => <DeckRow key={deck.id} {...deck} />)}*/}
+              {isSuccess && sortedData.map(deck => <DeckRow key={deck.id} {...deck} />)}
             </Table.Body>
           </Table.Root>
         </div>
