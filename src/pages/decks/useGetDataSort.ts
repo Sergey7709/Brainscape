014ca9bcc1fa387c483b70acd7d67a7fff42d048
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-04 11:56:22 +0300

    Add useGetDataSort.ts

---
+++ src/pages/decks/useGetDataSort.ts
@@ -0,0 +1,31 @@
+import { useMemo, useState } from 'react'
+
+import { Sort } from '@/components/ui/tables'
+import { useGetDecksQuery } from '@/service'
+
+export const useGetDataSort = () => {
+  const { data, isSuccess } = useGetDecksQuery()
+  const [sort, setSort] = useState<Sort>(null)
+  const sortString: string | null = sort ? `${sort?.key}-${sort?.direction}` : null
+
+  const sortedData = useMemo(() => {
+    if (isSuccess && !!data.items) {
+      if (!sortString) {
+        return data.items
+      }
+      const [key, direction] = sortString.split('-')
+
+      return [...data.items].sort((a, b) => {
+        if (direction === 'asc') {
+          return a[key as keyof typeof a] > b[key as keyof typeof b] ? 1 : -1
+        }
+
+        return a[key as keyof typeof a] < b[key as keyof typeof b] ? 1 : -1
+      })
+    } else {
+      return []
+    }
+  }, [sortString, data, isSuccess])
+
+  return { sort, setSort, isSuccess, sortedData }
+}
