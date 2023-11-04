7a661bc93fbb531eb497a56018f3be3528373710
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-04 12:47:34 +0300

    Add isLoading

--- src/pages/decks/useGetDataSort.ts
+++ src/pages/decks/useGetDataSort.ts
@@ -4,12 +4,24 @@ import { Sort } from '@/components/ui/tables'
 import { useGetDecksQuery } from '@/service'
 
 export const useGetDataSort = () => {
-  const { data, isSuccess } = useGetDecksQuery()
+  const { data, isSuccess, isLoading } = useGetDecksQuery()
   const [sort, setSort] = useState<Sort>(null)
   const sortString: string | null = sort ? `${sort?.key}-${sort?.direction}` : null
-
+  // const [key, direction] = sortString ? sortString.split('-') : []
   const sortedData = useMemo(() => {
     if (isSuccess && !!data.items) {
+      // if (!sortString) {
+      //   return data.items
+      // }
+      // const [key, direction] = sortString.split('-')
+      //
+      // return [...data.items].sort((a, b) => {
+      //   if (direction === 'asc') {
+      //     return a[key as keyof typeof a] > b[key as keyof typeof b] ? 1 : -1
+      //   }
+      //
+      //   return a[key as keyof typeof a] < b[key as keyof typeof b] ? 1 : -1
+      // })
       if (!sortString) {
         return data.items
       }
@@ -27,5 +39,5 @@ export const useGetDataSort = () => {
     }
   }, [sortString, data, isSuccess])
 
-  return { sort, setSort, isSuccess, sortedData }
+  return { sort, setSort, isSuccess, sortedData, isLoading }
 }
