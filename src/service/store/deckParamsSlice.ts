9307369d7969e9dae6198d2f1eb59134dd9bddaa
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-05 21:57:48 +0300

    Add minMaxCardsCountReducer

--- src/service/store/deckParamsSlice.ts
+++ src/service/store/deckParamsSlice.ts
@@ -1,13 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-
-const initialState = {
-  currentPage: 1,
-  itemsPerPage: 10,
-}
-
 type InitialState = {
   currentPage?: number
   itemsPerPage?: number
+  minMaxCardsCount?: number[] //!!!!!!!!! доработать типизацию
+}
+export const initialState = {
+  currentPage: 1,
+  itemsPerPage: 10,
+  minMaxCardsCount: [0, 50],
 }
 
 const deckParamsSlice = createSlice({
@@ -17,8 +17,11 @@ const deckParamsSlice = createSlice({
     currentPageReducer: (state, action: PayloadAction<InitialState>) => {
       state.currentPage = action.payload.currentPage ?? 1
     },
+    minMaxCardsCountReducer: (state, action: PayloadAction<InitialState>) => {
+      state.minMaxCardsCount = action.payload.minMaxCardsCount ?? [0, 50]
+    },
   },
 })
 
-export const { currentPageReducer } = deckParamsSlice.actions
+export const { currentPageReducer, minMaxCardsCountReducer } = deckParamsSlice.actions
 export default deckParamsSlice.reducer
