44d7f49997d295a034c7d9384d8a3353a9ae0bb1
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-06 23:03:17 +0300

    Add useCombineAppSelector, clearFilterReducer, useState for valueForSlider, handlerTextFieldChangeValue, handlerClearFilter, handleOnValueChange. Edit props for TextField and TabSwitcher and Slider, Button.

--- src/pages/decks/decks-panel/decks-panel.tsx
+++ src/pages/decks/decks-panel/decks-panel.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import s from './decks-panel.module.scss'
 
 import { Delete } from '@/assets/icons'
@@ -5,15 +7,25 @@ import { Button } from '@/components/ui/button'
 import { Slider } from '@/components/ui/slider'
 import { TabSwitcher } from '@/components/ui/tab-switcher'
 import { TextField } from '@/components/ui/textField'
+import { useCombineAppSelector } from '@/pages/decks/useCombineAppSelector.ts'
 import { useAppDispatch } from '@/service'
 import { maxCardsValue, minCardsValue } from '@/service/store/constantsForInitialValue.ts'
-import { minMaxCardsCountReducer } from '@/service/store/deckParamsSlice.ts'
+import { clearFilterReducer, minMaxCardsCountReducer } from '@/service/store/deckParamsSlice.ts'
 
 type DecksPanelProps = {
   handlerTabSwitchChangeValue: (value: string) => void
 }
 export const DecksPanel = ({ handlerTabSwitchChangeValue }: DecksPanelProps) => {
   const dispatch = useAppDispatch()
+
+  const {
+    minMaxCardsCount: [minCardsCount, maxCardsCount],
+    myOrAllAuthorCards,
+  } = useCombineAppSelector()
+
+  const [valueForSlider, setValueForSlider] = useState<number[]>(
+    [minCardsCount, maxCardsCount] || [minCardsValue, maxCardsValue]
+  )
 
   const classNames = {
     decksPanel: s.decksPanel,
@@ -26,18 +38,34 @@ export const DecksPanel = ({ handlerTabSwitchChangeValue }: DecksPanelProps) => 
     dispatch(minMaxCardsCountReducer({ minMaxCardsCount: value }))
   }
 
-  // const handlerTabSwitchChangeValue = (value: string) => {
-  //   console.log('valueTabSwitch', value)
-  // }
+  const handlerTextFieldChangeValue = (value: string) => {
+    console.log('TextFieldValue', value)
+  }
+
+  const handlerClearFilter = () => {
+    dispatch(clearFilterReducer())
+    setValueForSlider([minCardsValue, maxCardsValue])
+  }
+
+  const handleOnValueChange = (value: number[]) => {
+    setValueForSlider(value)
+  }
+
+  console.log('deck-panel')
+  console.log('min', minCardsCount, 'max', maxCardsCount)
 
   return (
     <div className={classNames.decksPanel}>
       <div className={classNames.searchInput}>
-        <TextField type={'search'} placeholder={'Input search'} />
+        <TextField
+          type={'search'}
+          placeholder={'Input search'}
+          onValueChange={handlerTextFieldChangeValue}
+        />
       </div>
       <TabSwitcher
         label={'Show packs cards'}
-        defaultValue={'allCards'}
+        value={myOrAllAuthorCards === 'allCards' ? 'allCards' : 'myCards'}
         tabData={[
           {
             title: 'My cards',
@@ -55,10 +83,14 @@ export const DecksPanel = ({ handlerTabSwitchChangeValue }: DecksPanelProps) => 
         label={'Number of cards'}
         min={minCardsValue}
         max={maxCardsValue}
-        defaultValue={[minCardsValue, maxCardsValue]}
+        defaultValue={[minCardsCount, maxCardsCount]}
+        // defaultValue={[minCardsValue, maxCardsValue]}
         onValueCommit={handlerSliderChangeValue}
+        // value={[minMaxCardsCount[0], minMaxCardsCount[1]]}
+        value={valueForSlider}
+        onValueChange={handleOnValueChange}
       />
-      <Button variant={'secondary'}>
+      <Button variant={'secondary'} onClick={handlerClearFilter}>
         <Delete /> Clear Filter
       </Button>
     </div>
