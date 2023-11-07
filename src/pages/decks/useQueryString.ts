b1448049b312714db5a6d5fbd7b6d19f68ed4f9f
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-07 12:19:49 +0300

    Add useQueryString.ts

---
+++ src/pages/decks/useQueryString.ts
@@ -0,0 +1,24 @@
+import { authorCardsIDAbsent, itemsPerPageValue, maxCardsValue, minCardsValue } from '@/service'
+
+type QueryString = {
+  currentPage: number
+  itemsPerPage: number
+  minMaxCardsCount: number[]
+  myOrAllAuthorCards: string
+}
+export const useQueryString = ({
+  currentPage,
+  itemsPerPage,
+  minMaxCardsCount,
+  myOrAllAuthorCards,
+}: QueryString) => {
+  return [
+    currentPage !== currentPage ? `currentPage=${currentPage}` : '',
+    itemsPerPage !== itemsPerPageValue ? `itemsPerPage=${itemsPerPage}` : '',
+    minMaxCardsCount[0] !== minCardsValue ? `minCardsCount=${minMaxCardsCount[0]}` : '',
+    minMaxCardsCount[1] !== maxCardsValue ? `maxCardsCount=${minMaxCardsCount[1]}` : '',
+    myOrAllAuthorCards !== authorCardsIDAbsent ? `authorId=${myOrAllAuthorCards}` : '',
+  ]
+    .filter(el => !!el)
+    .join('&')
+}
