3371ca6b16b27246eb78fbc92be61c042e85de36
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-07 17:31:48 +0300

    Edit maxCardsValue

--- src/service/store/constantsForInitialValue.ts
+++ src/service/store/constantsForInitialValue.ts
@@ -1,5 +1,5 @@
 export const minCardsValue = 0
-export const maxCardsValue = 99
+export const maxCardsValue = 100
 export const currentPageValue = 1
 export const itemsPerPageValue = 10
 export const authorCardsIDAbsent = 'allCards'
