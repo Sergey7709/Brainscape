88c4da2c5e62f7073b93c0cc44114ea8534a5dbe
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-08 13:12:40 +0300

    Add useDebounce, and dispatch reset on currentPage =1

--- src/pages/decks/decks-panel/decks-panel.tsx
+++ src/pages/decks/decks-panel/decks-panel.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import s from './decks-panel.module.scss'
 
@@ -10,7 +10,13 @@ import { TextField } from '@/components/ui/textField'
 import { useCombineAppSelector } from '@/pages/decks/useCombineAppSelector.ts'
 import { useAppDispatch } from '@/service'
 import { maxCardsValue, minCardsValue } from '@/service/store/constantsForInitialValue.ts'
-import { clearFilterReducer, minMaxCardsCountReducer } from '@/service/store/deckParamsSlice.ts'
+import {
+  clearFilterReducer,
+  currentPageReducer,
+  findNameReducer,
+  minMaxCardsCountReducer,
+} from '@/service/store/deckParamsSlice.ts'
+import { useDebounce } from '@/utils/functions/useDebounce.ts'
 
 type DecksPanelProps = {
   handlerTabSwitchChangeValue: (value: string) => void
@@ -27,69 +33,81 @@ export const DecksPanel = ({ handlerTabSwitchChangeValue }: DecksPanelProps) => 
     [minCardsCount, maxCardsCount] || [minCardsValue, maxCardsValue]
   )
 
+  const [searchValue, setSearchValue] = useState<string>('')
+  const debounce = useDebounce({ value: searchValue, milliSeconds: 700 })
+
+  useEffect(() => {
+    // console.log('TextFieldValue', debounce)
+    dispatch(findNameReducer({ findName: debounce }))
+    dispatch(currentPageReducer({ currentPage: 1 })) ///!!! Уточнить норм ли так переключать на 1 страницу?
+  }, [debounce])
+
   const classNames = {
     decksPanel: s.decksPanel,
     searchInput: s.searchInput,
     slider: s.slider,
   }
 
-  const handlerSliderChangeValue = (value: number[]) => {
-    console.log('valueSlider', value)
+  const handlerSliderCommitValue = (value: number[]) => {
+    // console.log('valueSlider', value)
     dispatch(minMaxCardsCountReducer({ minMaxCardsCount: value }))
+    dispatch(currentPageReducer({ currentPage: 1 })) ///!!! Уточнить норм ли так переключать на 1 страницу?
   }
 
-  const handlerTextFieldChangeValue = (value: string) => {
-    console.log('TextFieldValue', value)
-  }
+  const handlerTextFieldChangeValue = useCallback((value: string) => {
+    setSearchValue(value)
+  }, [])
 
   const handlerClearFilter = () => {
     dispatch(clearFilterReducer())
     setValueForSlider([minCardsValue, maxCardsValue])
   }
 
-  const handleOnValueChange = (value: number[]) => {
+  const handlerValueChangeSlider = (value: number[]) => {
     setValueForSlider(value)
   }
 
-  console.log('deck-panel')
-  console.log('min', minCardsCount, 'max', maxCardsCount)
+  // console.log('deck-panel')
+  // console.log('min', minCardsCount, 'max', maxCardsCount)
 
   return (
-    <div className={classNames.decksPanel}>
-      <div className={classNames.searchInput}>
-        <TextField
-          type={'search'}
-          placeholder={'Input search'}
-          onValueChange={handlerTextFieldChangeValue}
+    <>
+      <div className={classNames.decksPanel}>
+        <div className={classNames.searchInput}>
+          <TextField
+            type={'search'}
+            placeholder={'Input search'}
+            onValueChange={handlerTextFieldChangeValue}
+          />
+        </div>
+        <TabSwitcher
+          label={'Show packs cards'}
+          value={myOrAllAuthorCards === 'allCards' ? 'allCards' : 'myCards'}
+          tabData={[
+            {
+              title: 'My cards',
+              value: 'myCards',
+            },
+            {
+              title: 'All cards',
+              value: 'allCards',
+            },
+          ]}
+          onValueChange={handlerTabSwitchChangeValue}
         />
+        <Slider
+          className={classNames.slider}
+          label={'Number of cards'}
+          min={minCardsValue}
+          max={maxCardsValue}
+          onValueCommit={handlerSliderCommitValue}
+          value={valueForSlider}
+          onValueChange={handlerValueChangeSlider}
+        />
+        <Button variant={'secondary'} onClick={handlerClearFilter}>
+          <Delete /> Clear Filter
+        </Button>
       </div>
-      <TabSwitcher
-        label={'Show packs cards'}
-        value={myOrAllAuthorCards === 'allCards' ? 'allCards' : 'myCards'}
-        tabData={[
-          {
-            title: 'My cards',
-            value: 'myCards',
-          },
-          {
-            title: 'All cards',
-            value: 'allCards',
-          },
-        ]}
-        onValueChange={handlerTabSwitchChangeValue}
-      />
-      <Slider
-        className={classNames.slider}
-        label={'Number of cards'}
-        min={minCardsValue}
-        max={maxCardsValue}
-        onValueCommit={handlerSliderChangeValue}
-        value={valueForSlider}
-        onValueChange={handleOnValueChange}
-      />
-      <Button variant={'secondary'} onClick={handlerClearFilter}>
-        <Delete /> Clear Filter
-      </Button>
-    </div>
+    </>
   )
 }
