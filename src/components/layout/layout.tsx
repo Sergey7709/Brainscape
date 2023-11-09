9d046e2aaaf8173ea2f39bdb9eefaaf3bef4bfc2
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-09 20:36:44 +0300

    Add Loader

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -12,9 +12,23 @@ export const Layout = () => {
     container: s.container,
   }
 
+  // const { path } = useParams() ///!!!!
+
+  // const [searchParams] = useSearchParams() ///!!!!
+  //
+  // console.log('path', path) ///!!!!
+  // const currentParams = Object.fromEntries([...searchParams]) ///!!!!
+  //
+  // console.log(`?${new URLSearchParams(currentParams).toString()}`) ///!!!!
+
   const { isSuccess: isAuthenticated, isLoading, data } = useGetAuthUserMeDataQuery()
 
   const [getLogOut] = useLogoutUserMutation()
+
+  if (isLoading) {
+    return <Loader />
+  } //!!!!!!!!!!
+  console.log('layout')
 
   return (
     <div className={classNames.container}>
