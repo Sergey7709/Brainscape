2dbd3adbaaf6b75a0b002917b861894ab1ede0bb
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-09 20:38:36 +0300

    Add let searchParams = {} and useEffect

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -1,9 +1,41 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
 
 import { useGetAuthUserMeDataQuery } from '@/service'
 
+let searchParams = {} ///!!!!!!
+
 export const PrivateRoutes = () => {
+  const [search, setSearch] = useSearchParams() ///!!!!!!
+
   const { isSuccess: isAuthenticated } = useGetAuthUserMeDataQuery()
+
+  // if (search.toString().length) {
+  //   searchParams = search ///!!!!!!
+  //   console.log('search', search)
+  //   console.log(search.toString())
+  // }
+
+  // if (Object.keys(Object.fromEntries(search)).length) {
+  //   searchParams = Object.fromEntries(search) ///!!!!!!
+  //   // console.log('search', search)
+  // }
+  //
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     setSearch(searchParams)
+  //   }
+  // }, [isAuthenticated]) ///!!!!!!
+
+  useEffect(() => {
+    if (search.toString().length) {
+      searchParams = search ///!!!!!!
+      // console.log('search', search)
+      // console.log(search.toString())
+    }
+    setSearch(searchParams)
+  }, []) ///!!!!!!
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
