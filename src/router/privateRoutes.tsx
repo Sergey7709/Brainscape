a6d32f369eb2b060ee7a2a07b50e8e0aa3a9a801
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-10 15:34:04 +0300

    Add dispatch

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from 'react'
 
-import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
+import { Navigate, Outlet, useLocation, useSearchParams } from 'react-router-dom'
 
-import { useGetAuthUserMeDataQuery } from '@/service'
+import { useAppDispatch, useGetAuthUserMeDataQuery } from '@/service'
+import { searchParamsQuery } from '@/service/store/deckParamsSlice.ts'
 
 let searchParams = {} ///!!!!!!
 
 export const PrivateRoutes = () => {
-  const [search, setSearch] = useSearchParams() ///!!!!!!
+  const [search, setSearch] = useSearchParams()
+
+  const dispatch = useAppDispatch() ///!!!!!!
 
   const { isSuccess: isAuthenticated } = useGetAuthUserMeDataQuery()
-
+  // const location = useLocation()
   // if (search.toString().length) {
   //   searchParams = search ///!!!!!!
   //   console.log('search', search)
@@ -30,12 +33,14 @@ export const PrivateRoutes = () => {
 
   useEffect(() => {
     if (search.toString().length) {
-      searchParams = search ///!!!!!!
-      // console.log('search', search)
       // console.log(search.toString())
+      // console.log('location.pathname', location.pathname, '+', 'location.search', location.search)
+
+      dispatch(searchParamsQuery({ searchParamsQuery: search.toString() })) ///!!!!!!
     }
-    setSearch(searchParams)
-  }, []) ///!!!!!!
+    // setSearch(searchParams)
+    // setSearch(search)
+  }, [isAuthenticated])
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
