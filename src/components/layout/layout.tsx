80b01a38cfe8fa5f035cf9830035cdc525553fef
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-11 21:26:44 +0300

    test change layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,48 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
 
 import s from './layout.module.scss'
 
 import { Header } from '@/components/header'
 import { Loader } from '@/components/ui/loader'
 import { ScrollToTopButton } from '@/components/ui/scroll-to-top-button/scroll-to-top-button.tsx'
-import { useGetAuthUserMeDataQuery, useLogoutUserMutation } from '@/service'
+import {
+  useAppDispatch,
+  useAppSelector,
+  useGetAuthUserMeDataQuery,
+  useLogoutUserMutation,
+} from '@/service'
+import { searchParamsQuery } from '@/service/store/deckParamsSlice.ts'
 
 export const Layout = () => {
   const classNames = {
     container: s.container,
   }
 
+  ////!!!!!!!!!!!!
+  // const [search, setSearch] = useSearchParams()
+  // const dispatch = useAppDispatch() ///!!!!!!
+  // const queryState = useAppSelector(state => state.deckReducer.searchParamsQuery)
+  //
+  // // console.log('queryState1', queryState)
+  // const { isSuccess: isAuthenticated, isLoading, data, isFetching } = useGetAuthUserMeDataQuery()
+  //
+  // useEffect(() => {
+  //   if (search.toString().length) {
+  //     dispatch(searchParamsQuery({ searchParamsQuery: search.toString() })) ///!!!!!!
+  //     // setSearch(search)
+  //     // console.log('dispatch, setSearch')
+  //     // newSearch = search
+  //     // setSearch(newSearch)
+  //   }
+  //   // setSearch(newSearch)
+  //   setSearch(queryState)
+  // }, [isAuthenticated])
+  // // console.log('privatRouter', search.toString())
+  // // console.log('queryState2', queryState)
+
+  ////!!!!!!!!!!!!
   // const { path } = useParams() ///!!!!
 
   // const [searchParams] = useSearchParams() ///!!!!
@@ -25,21 +56,67 @@ export const Layout = () => {
 
   const [getLogOut] = useLogoutUserMutation()
 
-  // if (isLoading || isFetching) {
+  // if (isLoading) {
   //   return <Loader />
   // } //!!!!!!!!!!
-  console.log('layout')
+
+  // const [authMe, setAuthMe] = useState(false)
+  //
+  // // // const { isSuccess: isAuthenticated, isLoading, data, isFetching } = useGetAuthUserMeDataQuery()
+  // //
+  // useEffect(() => {
+  //   if (!authMe) {
+  //     setAuthMe(true)
+  //     // console.log('authMe1', authMe)
+  //   }
+  // }, [authMe])
+  // useEffect(() => {
+  //   setAuthMe(prevState => {
+  //     if (!prevState) {
+  //       // console.log('authMe1', !prevState)
+  //
+  //       return true
+  //     }
+  //
+  //     return prevState
+  //   })
+  // }, [])
+  //
+  // // console.log('authMe2', authMe)
+  //
+  // // const [getLogOut] = useLogoutUserMutation()
+  //
+  // if ((isLoading || isFetching) && !authMe) {
+  //   return <Loader />
+  // }
+  // console.log('isLoading', isLoading)
+  console.log('layout', isAuthenticated)
 
   return (
-    <div className={classNames.container}>
-      {isLoading && <Loader />}
-      <Header
-        isAuth={isAuthenticated}
-        user={data === null ? undefined : data}
-        onSignOut={getLogOut}
-      />
-      <Outlet />
-      <ScrollToTopButton />
-    </div>
+    <>
+      <div className={classNames.container}>
+        {isLoading && <Loader />}
+        <Header
+          isAuth={isAuthenticated}
+          user={data === null ? undefined : data}
+          onSignOut={getLogOut}
+        />
+        <Outlet />
+        <ScrollToTopButton />
+      </div>
+    </>
   )
+
+  // return (
+  //   <div className={classNames.container}>
+  //     {isLoading && <Loader />}
+  //     <Header
+  //       isAuth={isAuthenticated}
+  //       user={data === null ? undefined : data}
+  //       onSignOut={getLogOut}
+  //     />
+  //     <Outlet />
+  //     <ScrollToTopButton />
+  //   </div>
+  // )
 }
