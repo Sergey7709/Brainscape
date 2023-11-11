612d872e22da6273497c9447f2d78d08faa018a7
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-11 21:26:56 +0300

    test change privateRoutes.tsx

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -1,46 +1,45 @@
-import { useEffect } from 'react'
+import { memo, useEffect, useLayoutEffect } from 'react'
 
-import { Navigate, Outlet, useLocation, useSearchParams } from 'react-router-dom'
+import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
 
-import { useAppDispatch, useGetAuthUserMeDataQuery } from '@/service'
+import { Loader } from '@/components/ui/loader'
+import { useAppDispatch, useAppSelector, useGetAuthUserMeDataQuery } from '@/service'
 import { searchParamsQuery } from '@/service/store/deckParamsSlice.ts'
 
-let searchParams = {} ///!!!!!!
+// let newSearch = {}
 
-export const PrivateRoutes = () => {
+export const PrivateRoutes = memo(() => {
+  console.log('privatRouter start')
+
   const [search, setSearch] = useSearchParams()
-
-  const dispatch = useAppDispatch() ///!!!!!!
-
-  const { isSuccess: isAuthenticated } = useGetAuthUserMeDataQuery()
-  // const location = useLocation()
-  // if (search.toString().length) {
-  //   searchParams = search ///!!!!!!
-  //   console.log('search', search)
-  //   console.log(search.toString())
-  // }
-
-  // if (Object.keys(Object.fromEntries(search)).length) {
-  //   searchParams = Object.fromEntries(search) ///!!!!!!
-  //   // console.log('search', search)
-  // }
   //
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     setSearch(searchParams)
-  //   }
-  // }, [isAuthenticated]) ///!!!!!!
+  const dispatch = useAppDispatch() ///!!!!!!
+  const queryState = useAppSelector(state => state.deckReducer.searchParamsQuery)
+
+  //
+  // console.log('queryState1', queryState)
+  // console.log('privatRouter', search.toString())
+
+  const { isSuccess: isAuthenticated, isLoading } = useGetAuthUserMeDataQuery()
 
   useEffect(() => {
     if (search.toString().length) {
-      // console.log(search.toString())
-      // console.log('location.pathname', location.pathname, '+', 'location.search', location.search)
-
       dispatch(searchParamsQuery({ searchParamsQuery: search.toString() })) ///!!!!!!
+      // setSearch(search)
+      // console.log('dispatch, setSearch')
+      // newSearch = search
+      // setSearch(newSearch)
     }
-    // setSearch(searchParams)
-    // setSearch(search)
+    // setSearch(newSearch)
+    setSearch(queryState)
   }, [isAuthenticated])
+  // console.log('privatRouter', search.toString())
+  // console.log('queryState2', queryState)
+  // console.log('privatRouter')
+  // if (isLoading) {
+  //   return <Loader />
+  // } //!!!!!!!!!!
+  console.log('privatRouter JSX')
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
-}
+})
