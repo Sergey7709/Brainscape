9cf81eaf93162084756b86fd6035d0d58f29eaae
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-14 18:43:08 +0300

    Delete unused reduсers

--- src/pages/decks/useCombineAppSelector.ts
+++ src/pages/decks/useCombineAppSelector.ts
@@ -2,20 +2,8 @@ import { useAppSelector } from '@/service'
 
 export const useCombineAppSelector = () => {
   const currentPage = useAppSelector(state => state.deckReducer.currentPage)
-  const itemsPerPage = useAppSelector(state => state.deckReducer.itemsPerPage)
-  const minMaxCardsCount = useAppSelector(state => state.deckReducer.minMaxCardsCount)
-  const myOrAllAuthorCards = useAppSelector(state => state.deckReducer.authorCards)
-  const findName = useAppSelector(state => state.deckReducer.findName)
-  const orderBy = useAppSelector(state => state.deckReducer.orderBy)
-  const queryRedirect = useAppSelector(state => state.deckReducer.searchParamsQuery) ///!!!!! в комбайн
 
   return {
     currentPage,
-    itemsPerPage,
-    minMaxCardsCount,
-    myOrAllAuthorCards,
-    findName,
-    orderBy,
-    queryRedirect,
   }
 }
