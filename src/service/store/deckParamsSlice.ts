afb359f1962235e8cedeefd4c48072d6eb3c555f
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-14 18:39:38 +0300

    Delete unused reduсers

--- src/service/store/deckParamsSlice.ts
+++ src/service/store/deckParamsSlice.ts
@@ -1,36 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import { DeckType } from '@/service/decks/decks.types.ts'
-import {
-  authorCardsIDAbsent,
-  currentPageValue,
-  findNameValue,
-  itemsPerPageValue,
-  maxCardsValue,
-  minCardsValue,
-  orderByValue,
-  searchParamsQueryValue,
-} from '@/service/store/constantsForInitialValue.ts'
+import { currentPageValue } from '@/service/store/constantsForInitialValue.ts'
 
 type InitialState = {
   currentPage?: number
-  itemsPerPage?: number
-  minMaxCardsCount?: number[] //!!!!!!!!! доработать типизацию
-  authorCards?: string
-  findName?: string
-  orderBy?: string //!!!!!!!!! доработать типизацию
-  searchParamsQuery?: string ///!!!!!!!
-  sortedData?: DeckType[]
 }
 export const initialState = {
   currentPage: currentPageValue,
-  itemsPerPage: itemsPerPageValue,
-  minMaxCardsCount: [minCardsValue, maxCardsValue],
-  authorCards: authorCardsIDAbsent,
-  findName: findNameValue,
-  orderBy: orderByValue,
-  searchParamsQuery: searchParamsQueryValue, ///!!!!!!!
-  sortedData: [] as DeckType[],
 }
 
 const deckParamsSlice = createSlice({
@@ -40,36 +16,10 @@ const deckParamsSlice = createSlice({
     currentPageReducer: (state, action: PayloadAction<InitialState>) => {
       state.currentPage = action.payload.currentPage ?? currentPageValue
     },
-    minMaxCardsCountReducer: (state, action: PayloadAction<InitialState>) => {
-      state.minMaxCardsCount = action.payload.minMaxCardsCount ?? [minCardsValue, maxCardsValue]
-    },
-    myOrAllAuthorCardsReducer: (state, action: PayloadAction<InitialState>) => {
-      state.authorCards = action.payload.authorCards ?? authorCardsIDAbsent
-    },
-    findNameReducer: (state, action: PayloadAction<InitialState>) => {
-      state.findName = action.payload.findName ?? findNameValue
-    },
-    orderByReducer: (state, action: PayloadAction<InitialState>) => {
-      state.orderBy = action.payload.orderBy ?? orderByValue
-    },
-    // searchParamsQuery: (state, action: PayloadAction<InitialState>) => {
-    //   state.searchParamsQuery = action.payload.searchParamsQuery ?? searchParamsQueryValue
-    // }, //!!!!!!!!
-    sortedDataReducer: (state, action: PayloadAction<InitialState>) => {
-      state.sortedData = action.payload.sortedData ?? []
-    }, //!!!!!!!!
+
     clearFilterReducer: () => initialState,
   },
 })
 
-export const {
-  currentPageReducer,
-  minMaxCardsCountReducer,
-  myOrAllAuthorCardsReducer,
-  clearFilterReducer,
-  sortedDataReducer,
-  findNameReducer,
-  orderByReducer,
-  // searchParamsQuery, ///!!!!!
-} = deckParamsSlice.actions
+export const { currentPageReducer, clearFilterReducer } = deckParamsSlice.actions
 export default deckParamsSlice.reducer
