c2c52fb8c8996fd63e64d67d1669cac653c41e74
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-14 18:33:45 +0300

    Delete comments

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -1,44 +1,18 @@
-import { memo, useEffect, useLayoutEffect } from 'react'
+import { memo } from 'react'
 
-import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 
 import { Loader } from '@/components/ui/loader'
-import { useAppDispatch, useAppSelector, useGetAuthUserMeDataQuery } from '@/service'
-import { searchParamsQuery } from '@/service/store/deckParamsSlice.ts'
-
-// let newSearch = {}
+import { useGetAuthUserMeDataQuery } from '@/service'
 
 export const PrivateRoutes = memo(() => {
-  // console.log('privatRouter start')
-  // const [search, setSearch] = useSearchParams()
-  // const dispatch = useAppDispatch() ///!!!!!!
-  // const queryState = useAppSelector(state => state.deckReducer.searchParamsQuery)
-  //
   const { isSuccess: isAuthenticated, isLoading } = useGetAuthUserMeDataQuery()
 
-  // useEffect(() => {
-  //   if (search.toString().length) {
-  //     dispatch(searchParamsQuery({ searchParamsQuery: search.toString() })) ///!!!!!!
-  //   }
-  //
-  //   setSearch(queryState)
-  //   // }, [isAuthenticated])  ///!!! было
-  // }, [search])
-
-  // useLayoutEffect(() => {
-  //   if (search.toString().length) {
-  //     dispatch(searchParamsQuery({ searchParamsQuery: search.toString() })) ///!!!!!!
-  //   }
-  //
-  //   setSearch(queryState)
-  // }, [search]) ////!!!!
-  //
   if (isLoading) {
     return <Loader />
   } //!!!!!!!!!!
 
   console.log('privatRouter JSX')
-  // console.log('search.toString()', search.toString())
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 })
