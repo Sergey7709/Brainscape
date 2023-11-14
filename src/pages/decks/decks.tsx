0ef863948255d7617858513c785937eacdbef391
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-14 21:27:51 +0300

    Layout and basic logic are moved from the component to the DeckComposition component

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -8,110 +8,125 @@ import { Pagination } from '@/components/ui/pagination'
 import { Sort, Table } from '@/components/ui/tables/tables'
 import { Typography } from '@/components/ui/typography'
 import { columns } from '@/pages/decks/constantsDeck.ts'
+import { DeckComposition } from '@/pages/decks/deck-composition/deck-composition.tsx'
 import { DeckRow } from '@/pages/decks/deck-row/deck-row.tsx'
 import { DecksPanel } from '@/pages/decks/decks-panel/decks-panel.tsx'
 import { useGetDataSort } from '@/pages/decks/useGetDataSort.ts'
-import { useAppDispatch, useGetAuthUserMeDataQuery } from '@/service'
+import { useAppDispatch, useGetAuthUserMeDataQuery, useGetDecksQuery } from '@/service'
 import { currentPageReducer } from '@/service/store/deckParamsSlice.ts'
 import { utilityForSearchParamsEdit } from '@/utils'
 
 export const Decks = () => {
-  // console.log('Deck start')
-  const [searchParams, setSearchParams] = useSearchParams()
+  // const { isLoading, isFetching } = useGetDataSort()
 
-  const dispatch = useAppDispatch() ////!!!!!!!!!!!!! удалить
+  const [searchParams] = useSearchParams()
+  const resultQuery = `${searchParams.toString()}`
+  const { isLoading, isFetching } = useGetDecksQuery(resultQuery)
 
-  const { data: meData } = useGetAuthUserMeDataQuery()
-  const meID = meData?.id
-
-  const { sort, setSort, sortedData, isSuccess, isLoading, data, currentPage, isFetching } =
-    useGetDataSort()
-
-  const { itemsPerPage, totalItems, totalPages } = data?.pagination ?? {}
-
-  const classNames = {
-    container: s.container,
-    decksPanel: s.decksPanel,
-    searchInput: s.searchInput,
-    slider: s.slider,
-    tableWrapper: s.tableWrapper,
-    head: s.head,
-    deck: s.deck,
-    pagination: s.paginationWrapper,
-  }
-
-  const handlerPagination = (page: number) => {
-    dispatch(currentPageReducer({ currentPage: page }))
-    utilityForSearchParamsEdit({
-      searchParams,
-      setSearchParams,
-      param: 'currentPage',
-      valueForNewParam: page.toString() ?? '',
-    })
-  }
-
-  const handlerTabSwitchChangeValue = () => {
-    utilityForSearchParamsEdit({
-      searchParams,
-      setSearchParams,
-      param: 'authorId',
-      valueForNewParam: meID ?? '',
-    })
-  }
-
-  const handlerSortValue = (sort: Sort) => {
-    setSort(sort)
-    sort?.key && sort?.direction !== undefined
-      ? utilityForSearchParamsEdit({
-          searchParams,
-          setSearchParams,
-          param: 'orderBy',
-          valueForNewParam: `${sort?.key}-${sort?.direction}`,
-        })
-      : utilityForSearchParamsEdit({
-          searchParams,
-          setSearchParams,
-          param: 'orderBy',
-          valueForNewParam: '',
-        })
-  }
-
-  const pagination = !!totalPages && (
-    <Pagination
-      currentPage={currentPage}
-      pageSize={itemsPerPage ?? 0}
-      totalCount={totalItems ?? 0}
-      onPageChange={page => handlerPagination(page)}
-    />
-  )
-
+  console.log('Deck')
+  // const [searchParams, setSearchParams] = useSearchParams()
+  //
+  // const dispatch = useAppDispatch() ////!!!!!!!!!!!!! удалить
+  //
+  // const { data: meData } = useGetAuthUserMeDataQuery()
+  // const meID = meData?.id
+  //
+  // const { sort, setSort, sortedData, isSuccess, isLoading, data, currentPage, isFetching } =
+  //   useGetDataSort()
+  //
+  // const { itemsPerPage, totalItems, totalPages } = data?.pagination ?? {}
+  //
+  // const classNames = {
+  //   container: s.container,
+  //   decksPanel: s.decksPanel,
+  //   searchInput: s.searchInput,
+  //   slider: s.slider,
+  //   tableWrapper: s.tableWrapper,
+  //   head: s.head,
+  //   deck: s.deck,
+  //   pagination: s.paginationWrapper,
+  // }
+  //
+  // const handlerPagination = (page: number) => {
+  //   dispatch(currentPageReducer({ currentPage: page }))
+  //   utilityForSearchParamsEdit({
+  //     searchParams,
+  //     setSearchParams,
+  //     param: 'currentPage',
+  //     valueForNewParam: page.toString() ?? '',
+  //   })
+  // }
+  //
+  // const handlerTabSwitchChangeValue = () => {
+  //   utilityForSearchParamsEdit({
+  //     searchParams,
+  //     setSearchParams,
+  //     param: 'authorId',
+  //     valueForNewParam: meID ?? '',
+  //   })
+  // }
+  //
+  // const handlerSortValue = (sort: Sort) => {
+  //   setSort(sort)
+  //   sort?.key && sort?.direction !== undefined
+  //     ? utilityForSearchParamsEdit({
+  //         searchParams,
+  //         setSearchParams,
+  //         param: 'orderBy',
+  //         valueForNewParam: `${sort?.key}-${sort?.direction}`,
+  //       })
+  //     : utilityForSearchParamsEdit({
+  //         searchParams,
+  //         setSearchParams,
+  //         param: 'orderBy',
+  //         valueForNewParam: '',
+  //       })
+  // }
+  //
+  // const pagination = !!totalPages && (
+  //   <Pagination
+  //     currentPage={currentPage}
+  //     pageSize={itemsPerPage ?? 0}
+  //     totalCount={totalItems ?? 0}
+  //     onPageChange={page => handlerPagination(page)}
+  //   />
+  // )
+  //
+  // const sortedDataOrnNothing =
+  //   isSuccess && sortedData.length ? (
+  //     sortedData.map(deck => <DeckRow key={deck.id} {...deck} />)
+  //   ) : (
+  //     <div className={s.noData}>
+  //       <p className={s.textNoData}>Упс... данные отсутствуют</p>
+  //     </div>
+  //   )
+  //
   // if (isLoading || isFetching) {
   //   return <Loader />
   // } //!!!
-
-  console.log('Deck load', 'Deck return JSX')
+  //
+  // console.log('Deck load', 'Deck return JSX')
 
   return (
     <>
-      {(isLoading || isFetching) && <Loader />}
-      <div className={classNames.container}>
-        <div className={classNames.deck}>
-          <div className={classNames.head}>
-            <Typography variant={'large'}>Packs list</Typography>
-            <Button>Add new pack</Button>
-          </div>
-          <DecksPanel handlerTabSwitchChangeValue={handlerTabSwitchChangeValue} setSort={setSort} />
-          <div className={classNames.tableWrapper}>
-            <Table.Root>
-              <Table.Header columns={columns} sort={sort} onSort={handlerSortValue} />
-              <Table.Body>
-                {isSuccess && sortedData.map(deck => <DeckRow key={deck.id} {...deck} />)}
-              </Table.Body>
-            </Table.Root>
-          </div>
-        </div>
-        <div className={classNames.pagination}>{pagination}</div>
-      </div>
+      {isLoading || isFetching ? <Loader /> : <DeckComposition />}
+      {/*{(isLoading || isFetching) && <Loader />}*/}
+      {/*<div className={classNames.container}>*/}
+      {/*  <div className={classNames.deck}>*/}
+      {/*    <div className={classNames.head}>*/}
+      {/*      <Typography variant={'large'}>Packs list</Typography>*/}
+      {/*      <Button>Add new pack</Button>*/}
+      {/*    </div>*/}
+      {/*    <DecksPanel handlerTabSwitchChangeValue={handlerTabSwitchChangeValue} setSort={setSort} />*/}
+      {/*    <div className={classNames.tableWrapper}>*/}
+      {/*      <Table.Root>*/}
+      {/*        <Table.Header columns={columns} sort={sort} onSort={handlerSortValue} />*/}
+      {/*        <Table.Body>{sortedDataOrnNothing}</Table.Body>*/}
+      {/*      </Table.Root>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div className={classNames.pagination}>{pagination}</div>*/}
+      {/*</div>*/}
     </>
   )
 }
