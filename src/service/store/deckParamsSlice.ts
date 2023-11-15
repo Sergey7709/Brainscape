6c8f112fe2b70a00da15eef462001962bf129271
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-15 14:08:56 +0300

    Add minMaxCardsCountReducer, sortTableReducer,findNameReducer,

--- src/service/store/deckParamsSlice.ts
+++ src/service/store/deckParamsSlice.ts
@@ -1,12 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import { currentPageValue } from '@/utils/constants/constantsForInitialValue.ts'
+import { Sort } from '@/components/ui/tables'
+import {
+  currentPageValue,
+  findNameValue,
+  maxCardsValue,
+  minCardsValue,
+} from '@/utils/constants/constantsForInitialValue.ts'
 
 type InitialState = {
   currentPage?: number
+  findName?: string
+  minMaxCardsCount?: number[]
+  sortTable?: Sort
 }
 export const initialState = {
   currentPage: currentPageValue,
+  findName: findNameValue,
+  minMaxCardsCount: [minCardsValue, maxCardsValue],
+  sortTable: null as Sort, ///!!!!
 }
 
 const deckParamsSlice = createSlice({
@@ -16,10 +28,24 @@ const deckParamsSlice = createSlice({
     currentPageReducer: (state, action: PayloadAction<InitialState>) => {
       state.currentPage = action.payload.currentPage ?? currentPageValue
     },
-
+    findNameReducer: (state, action: PayloadAction<InitialState>) => {
+      state.findName = action.payload.findName ?? ''
+    },
+    minMaxCardsCountReducer: (state, action: PayloadAction<InitialState>) => {
+      state.minMaxCardsCount = action.payload.minMaxCardsCount ?? [minCardsValue, maxCardsValue]
+    },
+    sortTableReducer: (state, action: PayloadAction<InitialState>) => {
+      state.sortTable = action.payload.sortTable ?? null
+    },
     clearFilterReducer: () => initialState,
   },
 })
 
-export const { currentPageReducer, clearFilterReducer } = deckParamsSlice.actions
+export const {
+  currentPageReducer,
+  findNameReducer,
+  clearFilterReducer,
+  minMaxCardsCountReducer,
+  sortTableReducer,
+} = deckParamsSlice.actions
 export default deckParamsSlice.reducer
