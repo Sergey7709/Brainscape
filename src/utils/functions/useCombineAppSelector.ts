f7d7a6a4a8f2c34832a0ef47fd87293935e16e5c
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-15 14:11:42 +0300

    Add     searchValue,         valueForSlider,         sortTable,

--- src/utils/functions/useCombineAppSelector.ts
+++ src/utils/functions/useCombineAppSelector.ts
@@ -2,8 +2,14 @@ import { useAppSelector } from '@/service'
 
 export const useCombineAppSelector = () => {
   const currentPage = useAppSelector(state => state.deckReducer.currentPage)
+  const searchValue = useAppSelector(state => state.deckReducer.findName)
+  const valueForSlider = useAppSelector(state => state.deckReducer.minMaxCardsCount)
+  const sortTable = useAppSelector(state => state.deckReducer.sortTable)
 
   return {
     currentPage,
+    searchValue,
+    valueForSlider,
+    sortTable,
   }
 }
