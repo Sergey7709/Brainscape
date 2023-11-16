b8b0fea43134222543ace8d5dc3e2d542cdfa2e1
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-16 15:29:57 +0300

    Add useSearchParams, currentPageValue, itemsPerPageValue, paginationValueInURL, skeleton

--- src/pages/decks/deck-composition/deck-composition.tsx
+++ src/pages/decks/deck-composition/deck-composition.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react'
 
+import { useSearchParams } from 'react-router-dom'
+
 import s from './../decks.module.scss'
 
 import { Button } from '@/components/ui/button'
@@ -10,16 +12,18 @@ import { columns } from '@/pages/decks/constantsDeck.ts'
 import { DeckRow } from '@/pages/decks/deck-row/deck-row.tsx'
 import { DecksPanel } from '@/pages/decks/decks-panel/decks-panel.tsx'
 import { useGetDataSort } from '@/pages/decks/hooks-and-functions/useGetDataSort.ts'
-import { sortTableReducer, useAppDispatch } from '@/service'
+import { currentPageValue, itemsPerPageValue, sortTableReducer, useAppDispatch } from '@/service'
 import { currentPageReducer } from '@/service/store/deckParamsSlice.ts'
-import { useCombineAppSelector, useUtilityForSearchParamsEdit } from '@/utils'
+import { useUtilityForSearchParamsEdit } from '@/utils'
 
 export const DeckComposition = memo(() => {
-  const { currentPage } = useCombineAppSelector()
-
   const dispatch = useAppDispatch()
 
   const utilityForSearchParamsEdit = useUtilityForSearchParamsEdit()
+
+  const [searchParams] = useSearchParams()
+
+  const paginationValueInURL = Number(searchParams.get('currentPage')) || currentPageValue
 
   const { sort, sortedData, isSuccess, isFetching, data } = useGetDataSort()
 
@@ -56,7 +60,7 @@ export const DeckComposition = memo(() => {
 
   const pagination = !!totalPages && (
     <Pagination
-      currentPage={currentPage}
+      currentPage={paginationValueInURL}
       pageSize={itemsPerPage ?? 0}
       totalCount={totalItems ?? 0}
       onPageChange={page => handlerPagination(page)}
@@ -67,13 +71,34 @@ export const DeckComposition = memo(() => {
     (isSuccess &&
       sortedData.length &&
       sortedData.map(deck => <DeckRow key={deck.id} {...deck} />)) ||
-    (!isFetching && (
-      <tr>
+    (!isFetching && !sortedData.length && (
+      <tr className={s.td}>
         <td className={s.td} colSpan={5}>
           <p className={s.textNoData}>Упс... данные отсутствуют</p>
         </td>
       </tr>
     ))
+
+  const skeleton =
+    isFetching &&
+    new Array((!sortedData.length && 1) || itemsPerPageValue)
+      .fill(1)
+      .map((_, index) => (
+        <DeckRow
+          key={index}
+          name={'Loading'}
+          cardsCount={0}
+          updated={new Date()}
+          author={{ id: index.toString(), name: 'Loading' }}
+          userId={index.toString()}
+          shots={0}
+          rating={0}
+          cover={''}
+          id={index.toString()}
+          isPrivate={true}
+          created={''}
+        />
+      ))
 
   return (
     <div className={classNames.container}>
@@ -86,7 +111,11 @@ export const DeckComposition = memo(() => {
         <div className={classNames.tableWrapper}>
           <Table.Root>
             <Table.Header columns={columns} sort={sort} onSort={handlerSortValue} />
-            {!isFetching && isSuccess && <Table.Body>{sortedDataOrnNothing}</Table.Body>}
+            {!isFetching && isSuccess ? (
+              <Table.Body>{sortedDataOrnNothing}</Table.Body>
+            ) : (
+              <Table.Body>{skeleton}</Table.Body>
+            )}
           </Table.Root>
         </div>
       </div>
