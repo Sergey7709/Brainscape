30c70e1b7fdbe67ada32541f09f2e7840d19b27a
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-17 11:29:12 +0300

    Edit rout, replace Navigate to="/login" on Navigate to="/home"

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -14,5 +14,6 @@ export const PrivateRoutes = memo(() => {
 
   console.log('privatRouter JSX')
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+  // return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+  return isAuthenticated ? <Outlet /> : <Navigate to="/home" /> ///!!!!!!!!!!!!!!!!!!!!!! доделать роут
 })
