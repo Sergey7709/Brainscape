d3f928902168b101a92f6fcd0a71b78180672fe2
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-19 13:11:48 +0300

    Add window.scrollTo(0, 0) for to move elements to the top of the page when the component is refreshed

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -7,6 +7,7 @@ export const Decks = () => {
   const { isLoading, isFetching } = useGetDecksQuery(utilitySearchParams())
 
   console.log('Deck')
+  window.scrollTo(0, 0) ///!!!!!!!!!!
 
   return (
     <>
