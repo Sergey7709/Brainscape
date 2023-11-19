67950b7f2cb834c9caba26bcd497dcf9de23a966
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-19 13:18:18 +0300

    Commented const sortedData

--- src/pages/decks/hooks-and-functions/useDataSort.ts
+++ src/pages/decks/hooks-and-functions/useDataSort.ts
@@ -1,18 +1,15 @@
-import { useMemo } from 'react'
-
 import { useSearchParams } from 'react-router-dom'
 
 import { Sort } from '@/components/ui/tables'
-import { GetEntitiesResponse } from '@/service/common/types.ts'
-import { DeckType } from '@/service/decks/decks.types.ts'
 import { orderByValue } from '@/utils'
 
-type DataSort = {
-  isSuccess: boolean
-  data: (GetEntitiesResponse<DeckType> & { maxCardsCount: number }) | undefined
-}
-export const useDataSort = (props: DataSort) => {
-  const { isSuccess, data } = props
+// type DataSort = {
+//   isSuccess: boolean
+//   data: (GetEntitiesResponse<DeckType> & { maxCardsCount: number }) | undefined
+// }
+// export const useDataSort = (props: DataSort) => {
+export const useDataSort = () => {
+  // const { isSuccess, data } = props
 
   const [searchParams] = useSearchParams()
   const sortString = searchParams.get('orderBy') ?? orderByValue
@@ -20,27 +17,28 @@ export const useDataSort = (props: DataSort) => {
   const [key, direction] = sortString.split('-')
   const sort = { key, direction } as Sort
 
-  const sortedData = useMemo(() => {
-    if (isSuccess && data?.items) {
-      if (sortString) {
-        return [...data.items].sort((a, b) => {
-          const [key, direction] = sortString.split('-')
-          const isAsc = direction === 'asc'
+  // const sortedData = useMemo(() => {
+  //   if (isSuccess && data?.items) {
+  //     if (sortString) {
+  //       return [...data.items].sort((a, b) => {
+  //         const [key, direction] = sortString.split('-')
+  //         const isAsc = direction === 'asc'
+  //
+  //         const aValue = a[key as keyof typeof a]
+  //         const bValue = b[key as keyof typeof b]
+  //
+  //         const result = aValue > bValue ? 1 : -1
+  //
+  //         return isAsc ? result : -result
+  //       })
+  //     } else {
+  //       return data.items
+  //     }
+  //   } else {
+  //     return []
+  //   }
+  // }, [sortString, data])
 
-          const aValue = a[key as keyof typeof a]
-          const bValue = b[key as keyof typeof b]
-
-          const result = aValue > bValue ? 1 : -1
-
-          return isAsc ? result : -result
-        })
-      } else {
-        return data.items
-      }
-    } else {
-      return []
-    }
-  }, [sortString, data])
-
-  return { sortedData, sort }
+  // return { sortedData, sort }
+  return { sort }
 }
