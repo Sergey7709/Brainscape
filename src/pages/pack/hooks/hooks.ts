631061adbf09b3cecd3dcab3f0752d7a9203031a
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-28 18:46:08 +0300

    Add hooks.ts

---
+++ src/pages/pack/hooks/hooks.ts
@@ -0,0 +1,61 @@
+import { useParams, useSearchParams } from 'react-router-dom'
+
+import { useDataSort } from '@/pages/decks/hooks-and-functions/useDataSort.ts'
+import { utilitySearchParams } from '@/pages/decks/hooks-and-functions/utilitySearchParams.ts'
+import {
+  currentPageValue,
+  useGetAuthUserMeDataQuery,
+  useGetDeckByIdCardsQuery,
+  useGetDeckByIdQuery,
+} from '@/service'
+
+export const useGetDataForPack = () => {
+  const packId = useParams() ///!!!!??????
+
+  const {
+    isSuccess: isAuthenticated,
+    isLoading: isLoadingAuth,
+    data: dataMeId,
+  } = useGetAuthUserMeDataQuery()
+
+  const {
+    data: dataDeck,
+    isSuccess: isSuccessDeck,
+    isLoading: isLoadingDeck,
+    isFetching: isFetchingDeck,
+  } = useGetDeckByIdQuery(packId.id ?? '')
+
+  const {
+    data: dataCards,
+    isSuccess: isSuccessCards,
+    isLoading: isLoadingCards,
+    isFetching: isFetchingCards,
+  } = useGetDeckByIdCardsQuery({ id: packId.id ?? '', query: utilitySearchParams() })
+
+  const { itemsPerPage, totalItems, totalPages } = dataCards?.pagination ?? {}
+
+  const [searchParams] = useSearchParams()
+
+  const paginationValueInURL = Number(searchParams.get('currentPage')) || currentPageValue
+
+  const { sort } = useDataSort()
+
+  return {
+    isAuthenticated,
+    isLoadingAuth,
+    dataMeId,
+    dataDeck,
+    isLoadingDeck,
+    isFetchingDeck,
+    isSuccessDeck,
+    dataCards,
+    isLoadingCards,
+    isFetchingCards,
+    isSuccessCards,
+    itemsPerPage,
+    totalItems,
+    totalPages,
+    paginationValueInURL,
+    sort,
+  }
+}
