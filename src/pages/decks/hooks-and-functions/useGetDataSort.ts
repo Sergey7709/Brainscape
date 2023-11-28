0832c9d70a9671aebe11ed0685b143d1fa22a787
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-28 16:33:46 +0300

    Add comment in line 7

--- src/pages/decks/hooks-and-functions/useGetDataSort.ts
+++ src/pages/decks/hooks-and-functions/useGetDataSort.ts
@@ -4,6 +4,9 @@ import { useGetDecksQuery } from '@/service'
 
 export const useGetDataSort = () => {
   const { data, isSuccess, isLoading, isFetching } = useGetDecksQuery(utilitySearchParams())
+  // const { data, isSuccess, isLoading, isFetching } = useGetDecksQuery(
+  //   `${utilitySearchParams()}${'&itemsPerPage=10'}`
+  // )
 
   const { sort } = useDataSort()
 
