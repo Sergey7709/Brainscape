6600eac5eaa89c32ac7385d5420952983d7c7e94
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-29 16:36:12 +0300

    Edit, add useGetDataForPack

--- src/pages/pack/sortedPackData/sortedPackData.tsx
+++ src/pages/pack/sortedPackData/sortedPackData.tsx
@@ -1,10 +1,36 @@
+import { Loader } from '@/components/ui/loader'
+import { useGetDataForPack } from '@/pages/pack/hooks'
 import { PackRow } from '@/pages/pack/pack-row/pack-row.tsx'
-import { PackCards } from '@/service/decks/decks.types.ts'
+// import { PackCards } from '@/service/decks/decks.types.ts'
 
-type SortedPackDataProps = {
-  items: PackCards[]
-  rating: number
-}
-export const SortedPackData = ({ items, rating }: SortedPackDataProps) => {
-  return items.map(pack => <PackRow key={pack.id} rating={rating || 0} {...pack} />)
+// type SortedPackDataProps = {
+//   items: PackCards[]
+//   rating: number
+//   mePackCards: boolean
+// }
+export const SortedPackData = () => {
+  const {
+    // isLoadingAuth,
+    // dataMeId,
+    dataDeck,
+    mePackCards,
+    isLoadingDeck,
+    isFetchingDeck,
+    dataCards,
+    isLoadingCards,
+    isFetchingCards,
+    // isSuccessCards,
+    // itemsPerPage,
+    // totalItems,
+    // totalPages,
+    // paginationValueInURL,
+    // sort,
+  } = useGetDataForPack()
+
+  return dataCards?.items.map(pack => (
+    <>
+      {(isLoadingCards || isFetchingCards || isLoadingDeck || isFetchingDeck) && <Loader />}
+      <PackRow key={pack.id} rating={dataDeck?.rating || 0} mePackCards={mePackCards} {...pack} />
+    </>
+  ))
 }
