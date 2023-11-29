411c304ffb384e8024485001c04e81b85205b2b5
Sergey Koshelev <shivacharya2019@gmail.com>
2023-11-29 21:40:29 +0300

    Delete comments

--- src/pages/pack/pack.tsx
+++ src/pages/pack/pack.tsx
@@ -1,24 +1,12 @@
-import { useEffect } from 'react'
-
-import { useSearchParams } from 'react-router-dom'
-
 import s from './pack.module.scss'
 
 import { Loader } from '@/components/ui/loader'
 import { Pagination } from '@/components/ui/pagination'
-import { TextField } from '@/components/ui/textField'
 import { useGetDataForPack } from '@/pages/pack/hooks'
 import { PackPanel } from '@/pages/pack/packPanel'
 import { PackSearch } from '@/pages/pack/packSearch'
 import { TablePack } from '@/pages/pack/tablePack'
-import { packFindNameReducer, useAppDispatch } from '@/service'
-import {
-  milliSecondsValue,
-  useCombineAppSelector,
-  useDebounce,
-  useIsFirstRender,
-  useUtilityForSearchParamsEdit,
-} from '@/utils'
+import { useIsFirstRender, useUtilityForSearchParamsEdit } from '@/utils'
 
 export const Pack = () => {
   const {
@@ -36,26 +24,6 @@ export const Pack = () => {
 
   const isFirstRender = useIsFirstRender()
   const utilityForSearchParamsEdit = useUtilityForSearchParamsEdit()
-
-  // const dispatch = useAppDispatch()
-  // const { packSearchValue } = useCombineAppSelector()
-  // const [searchParams] = useSearchParams()
-  // const findText = searchParams.get('question') || ''
-  // const utilityForSearchParamsEdit = useUtilityForSearchParamsEdit()
-  // const debounce = useDebounce({ value: packSearchValue, milliSeconds: milliSecondsValue })
-  //
-  // useEffect(() => {
-  //   debounce !== findText &&
-  //     utilityForSearchParamsEdit({
-  //       param: 'question',
-  //       valueForNewParam: debounce ? debounce : [],
-  //     })
-  //   isFirstRender && dispatch(packFindNameReducer({ packFindName: findText }))
-  // }, [debounce])
-  //
-  // const handlerTextFieldChangeValue = (value: string) => {
-  //   dispatch(packFindNameReducer({ packFindName: value }))
-  // }
 
   const handlerPagination = (page: number) => {
     utilityForSearchParamsEdit({
@@ -78,14 +46,6 @@ export const Pack = () => {
         <div className={s.pack}>
           <PackPanel />
           <PackSearch />
-          {/*<div className={s.inputPackRowWrapper}>*/}
-          {/*  <TextField*/}
-          {/*    type={'search'}*/}
-          {/*    placeholder={'Input search'}*/}
-          {/*    onValueChange={handlerTextFieldChangeValue}*/}
-          {/*    value={isFirstRender ? findText : packSearchValue}*/}
-          {/*  />*/}
-          {/*</div>*/}
           <TablePack />
           <div className={s.paginationWrapperPack}>
             {paginationReady && (
