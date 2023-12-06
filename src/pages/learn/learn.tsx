f353223c6e1b3fac44bab0ad52d54bf05745aff8
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-06 18:49:37 +0300

    Add variable and custom hook isFirstRender, edit conditional render JSX

--- src/pages/learn/learn.tsx
+++ src/pages/learn/learn.tsx
@@ -11,26 +11,62 @@ import { Question } from '@/pages/learn/question'
 import { BackToDeckLink } from '@/pages/pack/backToDeckLink'
 import { useGetDataForPack } from '@/pages/pack/hooks/useGetDataForPack.ts'
 import { useGetRandomCardsQuery } from '@/service'
+import { useIsFirstRender } from '@/utils'
 
 export const Learn = () => {
-  const { dataDeck } = useGetDataForPack()
   const packId = useParams()
-
-  const { data, isLoading } = useGetRandomCardsQuery(packId.id ?? '')
+  const { data, isLoading, isFetching, isSuccess } = useGetRandomCardsQuery(packId.id ?? '')
+  const { dataDeck, isLoadingDeck, isFetchingDeck } = useGetDataForPack()
+  // const [postGradeAnswer, { isLoading: isLoadingPost }] = useGradeCardMutation()
 
   const [openAnswer, setOpenAnswer] = useState(false)
 
-  console.log('data', data?.id, 'data', dataDeck?.name, 'openAnswer', openAnswer)
+  // console.log('data', data?.id, 'data', dataDeck?.name, 'openAnswer', openAnswer)
+
+  const isFirstRender = useIsFirstRender()
+
+  // console.log(
+  //   'openAnswer',
+  //   openAnswer,
+  //   'isFirstRender',
+  //   isFirstRender,
+  //   'isSuccess',
+  //   isSuccess,
+  //   'isLoading',
+  //   isLoading,
+  //   'isFetching',
+  //   isFetching,
+  //   'dataDeck',
+  //   dataDeck,
+  //   'isLoadingDeck',
+  //   isLoadingDeck,
+  //   'isFetchingDeck ',
+  //   isFetchingDeck,
+  //   'isSuccessDeck ',
+  //   isSuccessDeck
+  //   // 'isLoadingPost'
+  //   // isLoadingPost
+  // )
+
+  // const render = !isLoading && !isFetching && !isSuccess
+
+  // if (isFirstRender || isLoading || isFetching || isLoadingPost) {
+  //   return <Loader />
+  // }
+
+  // if (isLoading || isFetching) {
+  //   return <Loader />
+  // }
 
   return (
     <>
-      {isLoading && <Loader />}
+      {(isLoading || isFetching || isLoadingDeck || isFetchingDeck) && <Loader />}
       <div className={s.learnContainer}>
         <div className={s.learnBackToDeckWrapper}>
           <BackToDeckLink className={s.linkBackToDeck} />
         </div>
         <Card className={s.learnCards}>
-          {!openAnswer ? (
+          {!openAnswer && !isFetching && !isLoading && isSuccess ? (
             <>
               <Question
                 nameCard={dataDeck?.name || ''}
@@ -39,21 +75,26 @@ export const Learn = () => {
                 shots={data?.shots || 0}
                 handlerLearn={setOpenAnswer}
                 isLoading={isLoading}
+                isFetching={isFetching}
               />
             </>
           ) : (
-            <>
-              <Answer
-                nameCard={dataDeck?.name || ''}
-                imageAnswer={data?.answerImg || ''}
-                answer={data?.answer || ''}
-                shots={data?.shots || 0}
-                handlerLearn={setOpenAnswer}
-                cardID={data?.id || ''}
-                deckID={dataDeck?.id || ''}
-                isLoading={isLoading}
-              />
-            </>
+            !isFirstRender &&
+            !isLoading && (
+              <>
+                <Answer
+                  nameCard={dataDeck?.name || ''}
+                  imageAnswer={data?.answerImg || ''}
+                  answer={data?.answer || ''}
+                  shots={data?.shots || 0}
+                  handlerLearn={setOpenAnswer}
+                  cardID={data?.id || ''}
+                  deckID={dataDeck?.id || ''}
+                  isLoading={isLoading}
+                  isFetching={isFetching}
+                />
+              </>
+            )
           )}
         </Card>
       </div>
