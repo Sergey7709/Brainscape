648282920b6d6c6ea2f83d93011f41f5853c8a4d
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-09 13:10:07 +0300

    Add zod validation and react-hook-form

--- src/pages/decks/deck-addNewPack/deck-addNewPack.tsx
+++ src/pages/decks/deck-addNewPack/deck-addNewPack.tsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
+import { ChangeEvent, useRef, useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useController, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import s from './deck-addNewPack.module.scss'
 
+import { ChangePhoto } from '@/assets/icons'
+import { SignInForm, signInSchema } from '@/components/auth/sign-in'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/check-box'
 import { ImageUploader } from '@/components/ui/imageUploader'
@@ -9,53 +15,137 @@ import { Modal, ModalConstructor } from '@/components/ui/modal'
 import { ModalProps } from '@/components/ui/modal/typeForModal.ts'
 import { TextField } from '@/components/ui/textField'
 import { Typography } from '@/components/ui/typography'
+import { useCreateDeckMutation } from '@/service'
+
+export const addNewPackSchema = z.object({
+  namePack: z
+    .string({
+      required_error: 'Name is required',
+    })
+    .trim()
+    .nonempty('Name is required')
+    .min(3, 'Name must be at least 3 characters')
+    .max(30),
+
+  privatePack: z.boolean().optional(),
+  imageCover: z.instanceof(FileList).optional(),
+})
+
+export type NewPackSchema = z.infer<typeof addNewPackSchema>
+
+const initialValues: NewPackSchema = {
+  imageCover: undefined,
+  namePack: '',
+  privatePack: false,
+}
 
 export const DeckAddNewPack = (props: ModalProps) => {
-  const [cover, setCover] = useState<string>('')
+  const [handlerAddNewPackSubmit] = useCreateDeckMutation()
 
-  const handlerAddCoverPack = (image: File) => {
-    setCover(URL.createObjectURL(image))
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<NewPackSchema>({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    resolver: zodResolver(addNewPackSchema),
+    defaultValues: initialValues,
+  })
+
+  const {
+    field: { value, onChange },
+  } = useController({
+    name: 'privatePack',
+    control,
+    defaultValue: false,
+  })
+
+  const [cover, setCover] = useState<File | null>(null)
+  const handleAddImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+
+    // console.log('file', file)
+    if (file) {
+      // onAddImage(file)
+      setCover(file)
+    }
+  }
+  const onHandleSubmitForm = handleSubmit((form: NewPackSchema) => {
+    // onHandleSubmit(form)
+    console.log('form', form)
+  })
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const handleInputRef = () => {
+    fileInputRef.current?.click()
   }
 
   return (
-    <Modal {...props}>
+    <Modal>
       <ModalConstructor.PortalAndOverlay>
-        <ModalConstructor.Head
-          borderBottomHeader={props.borderBottomHeader}
-          justifyContentHeader={props.justifyContentHeader}
-        >
-          <Typography as={'span'} variant={'large'}>
-            Add new pack
-          </Typography>
-        </ModalConstructor.Head>
-        <ModalConstructor.Body>
-          <div className={s.cover}>
-            <div className={s.imgCoverWrapper}>
-              {cover && <img className={s.imageCover} src={cover} alt="preview" />}
-            </div>
-            <div className={s.buttonCoverWrapper}>
-              <ImageUploader
-                onAddImage={handlerAddCoverPack}
-                classNameButton={s.modalAddButtonCover}
-                classNameInput={s.modalAddInputCover}
-              />
-            </div>
-          </div>
-          <TextField label={'Name Pack'}></TextField>
-          <Checkbox label={'Private Pack'} checked={false} />
-        </ModalConstructor.Body>
-        <ModalConstructor.Footer>
-          <Button variant={'secondary'}>
-            <Typography as={'span'} variant={'body2'}>
-              Cancel
-            </Typography>
-          </Button>
-          <Button variant={'primary'} fullWidth>
-            <Typography as={'span'} variant={'body2'}>
+        <form onSubmit={onHandleSubmitForm}>
+          <ModalConstructor.Head
+            borderBottomHeader={props.borderBottomHeader}
+            justifyContentHeader={props.justifyContentHeader}
+          >
+            <Typography as={'span'} variant={'large'}>
               Add new pack
             </Typography>
-          </Button>
-        </ModalConstructor.Footer>
+          </ModalConstructor.Head>
+          <ModalConstructor.Body>
+            <div className={s.cover}>
+              <div className={s.buttonCoverWrapper}>
+                {cover && (
+                  <img className={s.imageCover} src={URL.createObjectURL(cover)} alt="Not Image" />
+                )}
+                {/*<Button*/}
+                {/*  type={'button'}*/}
+                {/*  as={'button'}*/}
+                {/*  variant="secondary"*/}
+                {/*  className={s.modalAddButtonCover}*/}
+                {/*  onClick={handleInputRef}*/}
+                {/*>*/}
+                {/*  <ChangePhoto onClick={handleInputRef} />*/}
+                {/*</Button>*/}
+                {/*<ImageUploader*/}
+                {/*  classNameButton={s.modalAddButtonCover}*/}
+                {/*  classNameInput={s.modalAddInputCover}*/}
+                {/*  classNameImg={s.imageCover}*/}
+                {/*  {...register('imageCover')}*/}
+                {/*  errorMessage={errors.imageCover?.message}*/}
+                {/*/>*/}
+                <TextField
+                  type={'file'}
+                  label={'imageCover'}
+                  // className={s.modalAddInputCover}
+                  {...register('imageCover')}
+                  // ref={fileInputRef}
+                  onChange={handleAddImage}
+                />
+              </div>
+            </div>
+            <TextField
+              label={'Name Pack'}
+              {...register('namePack')}
+              errorMessage={errors.namePack?.message}
+            ></TextField>
+            <Checkbox label={'Private Pack'} checked={value} onChange={onChange} />
+          </ModalConstructor.Body>
+          <ModalConstructor.Footer>
+            <Button variant={'secondary'}>
+              <Typography as={'span'} variant={'body2'}>
+                Cancel
+              </Typography>
+            </Button>
+            <Button variant={'primary'} fullWidth>
+              <Typography as={'span'} variant={'body2'}>
+                Add new pack
+              </Typography>
+            </Button>
+          </ModalConstructor.Footer>
+        </form>
       </ModalConstructor.PortalAndOverlay>
       <ModalConstructor.Trigger>
         <Button as={'button'} variant={'primary'}>
