e2ba127a743467af81f826504d6795bccbe7d342
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-12 16:51:11 +0300

    Add open, setOpen in useState and handlerClosedModal function for controlled modal window. Delete comments.

--- src/pages/decks/deck-addNewPack/deck-addNewPack.tsx
+++ src/pages/decks/deck-addNewPack/deck-addNewPack.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useController, useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -36,6 +38,8 @@ const initialValues: NewPackSchema = {
 export const DeckAddNewPack = (props: ModalProps) => {
   const [handlerAddNewPackSubmit] = useCreateDeckMutation()
 
+  const [open, setOpen] = useState(false)
+
   const {
     control,
     handleSubmit,
@@ -62,8 +66,13 @@ export const DeckAddNewPack = (props: ModalProps) => {
     console.log('form', form)
   })
 
+  const handlerClosedModal = () => {
+    console.log(open)
+    setOpen(!open)
+  }
+
   return (
-    <Modal>
+    <Modal open={open} setOpen={setOpen}>
       <ModalConstructor.PortalAndOverlay>
         <form onSubmit={onHandleSubmitForm}>
           <ModalConstructor.Head
@@ -84,7 +93,7 @@ export const DeckAddNewPack = (props: ModalProps) => {
             <Checkbox label={'Private Pack'} checked={value} onChange={onChange} />
           </ModalConstructor.Body>
           <ModalConstructor.Footer>
-            <Button type={'button'} variant={'secondary'}>
+            <Button type={'button'} variant={'secondary'} onClick={handlerClosedModal}>
               <Typography as={'span'} variant={'body2'}>
                 Cancel
               </Typography>
@@ -105,112 +114,3 @@ export const DeckAddNewPack = (props: ModalProps) => {
     </Modal>
   )
 }
-
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import { useController, useForm } from 'react-hook-form'
-// import { z } from 'zod'
-//
-// import s from './deck-addNewPack.module.scss'
-//
-// import { Button } from '@/components/ui/button'
-// import { Checkbox } from '@/components/ui/check-box'
-// import { ImageUploader } from '@/components/ui/imageUploader'
-// import { Modal, ModalConstructor } from '@/components/ui/modal'
-// import { ModalProps } from '@/components/ui/modal/typeForModal.ts'
-// import { TextField } from '@/components/ui/textField'
-// import { Typography } from '@/components/ui/typography'
-// import { useCreateDeckMutation } from '@/service'
-//
-// export const addNewPackSchema = z.object({
-//   namePack: z
-//     .string({
-//       required_error: 'Name is required',
-//     })
-//     .trim()
-//     .nonempty('Name is required')
-//     .min(3, 'Name must be at least 3 characters')
-//     .max(30),
-//
-//   privatePack: z.boolean().optional(),
-//   imageCover: z.instanceof(FileList).optional(),
-// })
-//
-// export type NewPackSchema = z.infer<typeof addNewPackSchema>
-//
-// const initialValues: NewPackSchema = {
-//   imageCover: undefined,
-//   namePack: '',
-//   privatePack: false,
-// }
-//
-// export const DeckAddNewPack = (props: ModalProps) => {
-//   const [handlerAddNewPackSubmit] = useCreateDeckMutation()
-//
-//   const {
-//     control,
-//     handleSubmit,
-//     register,
-//     formState: { errors },
-//   } = useForm<NewPackSchema>({
-//     mode: 'onBlur',
-//     reValidateMode: 'onBlur',
-//     resolver: zodResolver(addNewPackSchema),
-//     defaultValues: initialValues,
-//   })
-//
-//   const {
-//     field: { value, onChange },
-//   } = useController({
-//     name: 'privatePack',
-//     control,
-//     defaultValue: false,
-//   })
-//
-//   const onHandleSubmitForm = handleSubmit((form: NewPackSchema) => {
-//     // onHandleSubmit(form)
-//     console.log('form', form)
-//   })
-//
-//   return (
-//     <Modal>
-//       <ModalConstructor.PortalAndOverlay>
-//         <form onSubmit={onHandleSubmitForm}>
-//           <ModalConstructor.Head
-//             borderBottomHeader={props.borderBottomHeader}
-//             justifyContentHeader={props.justifyContentHeader}
-//           >
-//             <Typography as={'span'} variant={'large'}>
-//               Add new pack
-//             </Typography>
-//           </ModalConstructor.Head>
-//           <ModalConstructor.Body>
-//             <ImageUploader name={'imageCover'} register={register} />
-//             <TextField
-//               label={'Name Pack'}
-//               {...register('namePack')}
-//               errorMessage={errors.namePack?.message}
-//             ></TextField>
-//             <Checkbox label={'Private Pack'} checked={value} onChange={onChange} />
-//           </ModalConstructor.Body>
-//           <ModalConstructor.Footer>
-//             <Button type={'button'} variant={'secondary'}>
-//               <Typography as={'span'} variant={'body2'}>
-//                 Cancel
-//               </Typography>
-//             </Button>
-//             <Button variant={'primary'} fullWidth>
-//               <Typography as={'span'} variant={'body2'}>
-//                 Add new pack
-//               </Typography>
-//             </Button>
-//           </ModalConstructor.Footer>
-//         </form>
-//       </ModalConstructor.PortalAndOverlay>
-//       <ModalConstructor.Trigger>
-//         <Button as={'button'} variant={'primary'}>
-//           Add new pack
-//         </Button>
-//       </ModalConstructor.Trigger>
-//     </Modal>
-//   )
-// }
