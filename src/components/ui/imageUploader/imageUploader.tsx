c283c638112fb9f1f5825733a43bf0eee126f5b4
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-12 16:51:25 +0300

    Delete comments.

--- src/components/ui/imageUploader/imageUploader.tsx
+++ src/components/ui/imageUploader/imageUploader.tsx
@@ -21,9 +21,7 @@ export const ImageUploader = ({ register, setValue }: ImageAdderProps) => {
   const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
 
-    // console.log('file', file)
     if (file) {
-      // console.log(event.target.files)
       setCover(URL.createObjectURL(file))
       setValue('imageCover', event.currentTarget.files || undefined)
     } else {
@@ -33,7 +31,6 @@ export const ImageUploader = ({ register, setValue }: ImageAdderProps) => {
   }
 
   const onUpload = () => {
-    // console.log(hiddenInputRef)
     hiddenInputRef.current?.click()
   }
 
@@ -59,7 +56,6 @@ export const ImageUploader = ({ register, setValue }: ImageAdderProps) => {
         {cover ? (
           <>
             <img className={s.modalAddImageCover} src={cover} alt="Not Image" />
-            {/*<span className={s.tooltip}>Change</span>*/}
             <Typography variant={'subtitle2'} className={s.textCoverPreview}>
               This preview in cover, click to change
             </Typography>
@@ -79,82 +75,3 @@ export const ImageUploader = ({ register, setValue }: ImageAdderProps) => {
     </div>
   )
 }
-
-// import { ChangeEvent, ComponentPropsWithoutRef, useRef, useState } from 'react'
-//
-// import { UseFormRegister } from 'react-hook-form'
-//
-// import s from './imageUploader.module.scss'
-//
-// import { ChangePhoto } from '@/assets/icons'
-// import { Button } from '@/components/ui/button'
-// import { Typography } from '@/components/ui/typography'
-//
-// type ImageAdderProps = {
-//   register: UseFormRegister<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
-// } & ComponentPropsWithoutRef<'input'>
-//
-// export const ImageUploader = ({ register }: ImageAdderProps) => {
-//   const hiddenInputRef = useRef<HTMLInputElement | null>(null)
-//
-//   const [cover, setCover] = useState<string>('')
-//
-//   const { ref: registerRef, ...rest } = register('imageCover')
-//
-//   const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
-//     const file = event.target.files?.[0]
-//
-//     console.log('file', file)
-//     if (file) {
-//       setCover(URL.createObjectURL(file))
-//     } else {
-//       setCover('')
-//     }
-//     // file && setCover(URL.createObjectURL(file))
-//   }
-//
-//   const onUpload = () => {
-//     hiddenInputRef.current?.click()
-//   }
-//
-//   return (
-//     <div className={s.modalAddWrapper}>
-//       <input
-//         ref={e => {
-//           // console.log(registerRef(e))
-//           registerRef(e)
-//           hiddenInputRef.current = e
-//         }}
-//         {...rest}
-//         type="file"
-//         accept="image/*"
-//         className={s.modalAddInputCover}
-//         onChange={handleUploadedFile}
-//       />
-//       <Button
-//         as={'button'}
-//         type={'button'}
-//         variant="secondary"
-//         className={s.modalAddButtonCover}
-//         onClick={onUpload}
-//       >
-//         {cover ? (
-//           <>
-//             <img className={s.modalAddImageCover} src={cover} alt="Not Image" />
-//             <span className={s.tooltip}>Change</span>
-//           </>
-//         ) : (
-//           <>
-//             <ChangePhoto onClick={() => {}} />
-//             Add cover
-//           </>
-//         )}
-//       </Button>
-//       {cover && (
-//         <Typography variant={'body2'} className={s.deleteCover} onClick={handleUploadedFile}>
-//           ❌
-//         </Typography>
-//       )}
-//     </div>
-//   )
-// }
