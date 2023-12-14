c8c5678d4e273db335aec8cf563a8f88e73e3dec
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-14 21:37:43 +0300

    Add modalAddOREditPack.tsx

---
+++ src/pages/decks/deck-modal-pack/modalAddOREditPack.tsx
@@ -0,0 +1,118 @@
+import { BaseSyntheticEvent, Dispatch, SetStateAction } from 'react'
+
+import { FieldErrors, UseFormClearErrors, UseFormRegister, UseFormSetValue } from 'react-hook-form'
+
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/check-box'
+import { ImageUploader } from '@/components/ui/imageUploader'
+import { Modal, ModalConstructor } from '@/components/ui/modal'
+import { JustifyContent } from '@/components/ui/modal/typeForModal.ts'
+import { TextField } from '@/components/ui/textField'
+import { Typography } from '@/components/ui/typography'
+
+type ModalAddOREditPackProps = {
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+  onHandleSubmitForm: (e: BaseSyntheticEvent | undefined) => Promise<void>
+  borderBottomHeader?: boolean | undefined
+  justifyContentHeader?: JustifyContent | undefined
+  register: UseFormRegister<{
+    namePack: string
+    privatePack?: boolean | undefined
+    imageCover?: FileList | undefined
+  }>
+  errors: FieldErrors<{
+    namePack: string
+    privatePack?: boolean | undefined
+    imageCover?: FileList | undefined
+  }>
+  clearErrors: UseFormClearErrors<{
+    namePack: string
+    privatePack?: boolean | undefined
+    imageCover?: FileList | undefined
+  }>
+  setValue: UseFormSetValue<{
+    namePack: string
+    privatePack?: boolean | undefined
+    imageCover?: FileList | undefined
+  }>
+  cover: string
+  setCover: Dispatch<SetStateAction<string>>
+  nameValue: string
+  handlerNameChange: (value: string) => void
+  value: any
+  onChange: (...event: any[]) => void
+  handlerClosedModal: () => void
+}
+export const ModalAddOREditPack = (props: ModalAddOREditPackProps) => {
+  const {
+    open,
+    setOpen,
+    onHandleSubmitForm,
+    register,
+    errors,
+    clearErrors,
+    cover,
+    setCover,
+    setValue,
+    nameValue,
+    value,
+    onChange,
+    handlerNameChange,
+    handlerClosedModal,
+    borderBottomHeader,
+    justifyContentHeader,
+  } = props
+
+  return (
+    <Modal open={open} setOpen={setOpen}>
+      <ModalConstructor.PortalAndOverlay>
+        <form onSubmit={onHandleSubmitForm}>
+          <ModalConstructor.Head
+            borderBottomHeader={borderBottomHeader}
+            justifyContentHeader={justifyContentHeader}
+          >
+            <Typography as={'span'} variant={'large'}>
+              Add new pack
+            </Typography>
+          </ModalConstructor.Head>
+          <ModalConstructor.Body>
+            <ImageUploader
+              register={register}
+              setValue={setValue}
+              cover={cover}
+              setCover={setCover}
+              errorMessage={errors.imageCover?.message?.toString()}
+              clearErrors={clearErrors}
+            />
+            <TextField
+              value={nameValue}
+              onValueChange={handlerNameChange}
+              label={'Name Pack'}
+              {...register('namePack')}
+              errorMessage={errors.namePack?.message}
+            ></TextField>
+            <Checkbox label={'Private Pack'} checked={value} onChange={onChange} />
+          </ModalConstructor.Body>
+          <ModalConstructor.Footer>
+            <Button type={'button'} variant={'secondary'} onClick={handlerClosedModal}>
+              <Typography as={'span'} variant={'body2'}>
+                Cancel
+              </Typography>
+            </Button>
+            <Button variant={'primary'} fullWidth>
+              <Typography as={'span'} variant={'body2'}>
+                Add new pack
+              </Typography>
+            </Button>
+          </ModalConstructor.Footer>
+        </form>
+      </ModalConstructor.PortalAndOverlay>
+      <ModalConstructor.Trigger>
+        <Button as={'button'} variant={'primary'}>
+          Add new pack
+        </Button>
+      </ModalConstructor.Trigger>
+    </Modal>
+  )
+}
