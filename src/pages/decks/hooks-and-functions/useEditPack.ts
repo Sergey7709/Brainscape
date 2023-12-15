1bd8611ac3a7810a993ec0932349c02a585312db
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-15 18:52:37 +0300

    Add useEditPack.ts

---
+++ src/pages/decks/hooks-and-functions/useEditPack.ts
@@ -0,0 +1,22 @@
+import { toast } from 'react-toastify'
+
+import { useUpdateDeckMutation } from '@/service'
+
+export const useEditPack = () => {
+  const [handlerEditPackSubmit, isLoading] = useUpdateDeckMutation()
+
+  const utilityEditPack = (id: string, body: FormData) => {
+    handlerEditPackSubmit({ id, body })
+      .unwrap()
+      // .then(response => {
+      //   if (response && response.id) {
+      //     toast.success('A new package has been added!')
+      //   }
+      // })
+      .catch(error => {
+        toast.error('Failed edit pack!:', error)
+      })
+  }
+
+  return { utilityEditPack, isLoading }
+}
