6f321a6a6a20152fcce7ce458213adcd2d4f13fc
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-17 16:39:40 +0300

    Edit utilityDeletePack

--- src/pages/decks/hooks-and-functions/useDeletePack.ts
+++ src/pages/decks/hooks-and-functions/useDeletePack.ts
@@ -1,18 +1,22 @@
 import { toast } from 'react-toastify'
 
+import { useNavigateBackToDeck } from '@/pages/decks/hooks-and-functions/useNavigateBackToDeck.ts'
 import { useDeleteDeckMutation } from '@/service'
 
 export const useDeletePack = (name: string) => {
   const [deletePack, isLoading] = useDeleteDeckMutation()
 
+  const { navigateBackToDeck } = useNavigateBackToDeck()
+
   const utilityDeletePack = (id: string) => {
     deletePack(id)
       .unwrap()
-      // .then(response => {
-      //   if (response && response.id) {
-      //     toast.success(`The pack ${name} has been successfully deleted!`)
-      //   }
-      // })
+      .then(response => {
+        if (response && response.id) {
+          // toast.success(`The pack ${name} has been successfully deleted!`)
+          navigateBackToDeck()
+        }
+      })
       .catch(error => {
         toast.error(`Failed to delete: ${name} `, error)
       })
