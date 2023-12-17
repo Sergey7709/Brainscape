e84a1e8cf1d5898899df6a5f99fbf40cb5f568a3
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-17 16:39:16 +0300

    Add variables: utilityDeletePack, openModalDelete, openEditModal. Add function: handlerDeletePack, handlerOpenModal, handlerEditModal.Add components in JSX  PackDropDownMenu, ModalDeletePack, DeckEditPack.

--- src/pages/pack/packPanel/packPanel.tsx
+++ src/pages/pack/packPanel/packPanel.tsx
@@ -1,13 +1,36 @@
-import { Elipse } from '@/assets/icons/elipse.tsx'
-import { MoreVerticalOutline } from '@/assets/icons/more-vertical-outline.tsx'
+import { useState } from 'react'
+
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
+import { DeckEditPack } from '@/pages/decks/deck-editPack'
+import { ModalDeletePack } from '@/pages/decks/deck-modal-delete-pack'
+import { useDeletePack } from '@/pages/decks/hooks-and-functions'
 import { BackToDeckLink } from '@/pages/pack/backToDeckLink'
 import { useGetDataForPack } from '@/pages/pack/hooks'
 import s from '@/pages/pack/pack.module.scss'
+import { PackDropDownMenu } from '@/pages/pack/packDropDown'
 
 export const PackPanel = () => {
   const { dataDeck, mePackCards } = useGetDataForPack()
+
+  const { utilityDeletePack } = useDeletePack(dataDeck?.name || '')
+
+  const [openModalDelete, setOpenModalDelete] = useState(false)
+
+  const [openEditModal, setOpenEditModal] = useState(false)
+
+  const handlerDeletePack = () => {
+    utilityDeletePack(dataDeck?.id || '')
+    setOpenModalDelete(!openModalDelete)
+  }
+
+  const handlerOpenModal = () => {
+    setOpenModalDelete(!openModalDelete)
+  }
+
+  const handlerEditModal = () => {
+    setOpenEditModal(!openEditModal)
+  }
 
   return (
     <>
@@ -17,17 +40,44 @@ export const PackPanel = () => {
           {mePackCards ? (
             <>
               <Typography variant={'large'}>{`My Pack: "${dataDeck?.name || ''}"`}</Typography>
-              <Elipse className={s.packDropDown}>
-                <MoreVerticalOutline />
-              </Elipse>
+              <PackDropDownMenu
+                id={dataDeck?.id || ''}
+                handlerEditModal={handlerEditModal}
+                handlerOpenModal={handlerOpenModal}
+                cardsCount={dataDeck?.cardsCount || 0}
+              />
             </>
           ) : (
             <Typography variant={'large'}>{`Friend’s Pack: "${dataDeck?.name || ''}"`}</Typography>
           )}
         </div>
-        <Button className={s.packButton}>Add New Card</Button>
+        {mePackCards ? (
+          <Button className={s.packButton}>Add New Card</Button>
+        ) : (
+          <Button className={s.packButton}>Learn to Pack</Button>
+        )}
       </div>
-      <img src={dataDeck?.cover} alt={'Not found'} className={s.packImg} />
+      <img
+        src={dataDeck?.cover}
+        alt={'Not found'}
+        className={dataDeck?.cover ? s.packImg : s.packImgNone}
+      />
+      <ModalDeletePack
+        open={openModalDelete}
+        setOpen={setOpenModalDelete}
+        handlerClosedModal={handlerOpenModal}
+        handlerDeletePack={handlerDeletePack}
+      />
+      {dataDeck && (
+        <DeckEditPack
+          id={dataDeck.id}
+          coverPack={dataDeck.cover}
+          titlePack={dataDeck.name}
+          open={openEditModal}
+          setOpen={setOpenEditModal}
+          isPrivate={dataDeck.isPrivate}
+        />
+      )}
     </>
   )
 }
