d9965b97afc31e1919cfe682df4fb0cac870ad72
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-18 18:09:27 +0300

    Add useParams and conditional transfer to `/pack/${id}`

--- src/pages/decks/hooks-and-functions/useNavigateBackToDeck.ts
+++ src/pages/decks/hooks-and-functions/useNavigateBackToDeck.ts
@@ -1,14 +1,17 @@
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
 export const useNavigateBackToDeck = () => {
   const navigate = useNavigate()
   const location = useLocation()
+  const { id } = useParams()
 
   const navigateBackToDeck = () => {
     const urlDeck = sessionStorage.getItem('previousPath')
 
     if (location.pathname.includes('deck')) {
       return
+    } else if (location.pathname.includes('learn')) {
+      navigate(`/pack/${id}`)
     } else {
       navigate(`/deck${urlDeck}`)
     }
