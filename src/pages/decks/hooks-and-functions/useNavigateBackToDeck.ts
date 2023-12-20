b596d2494d49dcfc7298976e434cd47a3fee0851
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-20 11:50:45 +0300

    Edit navigateBackToDeck

--- src/pages/decks/hooks-and-functions/useNavigateBackToDeck.ts
+++ src/pages/decks/hooks-and-functions/useNavigateBackToDeck.ts
@@ -1,18 +1,20 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export const useNavigateBackToDeck = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  const { id } = useParams()
+  // const { id } = useParams()
 
   const navigateBackToDeck = () => {
     const urlDeck = sessionStorage.getItem('previousPath')
 
     if (location.pathname.includes('deck')) {
       return
-    } else if (location.pathname.includes('learn')) {
-      navigate(`/pack/${id}`)
-    } else {
+    }
+    // else if (location.pathname.includes('learn')) {
+    //   navigate(`/pack/${id}`)
+    // }
+    else {
       navigate(`/deck${urlDeck}`)
     }
   }
