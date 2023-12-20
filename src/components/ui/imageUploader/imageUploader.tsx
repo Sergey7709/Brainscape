1b96e5921d3e195ce724487b7dee65fec2960145
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-20 11:47:26 +0300

    Edit type and props component

--- src/components/ui/imageUploader/imageUploader.tsx
+++ src/components/ui/imageUploader/imageUploader.tsx
@@ -17,19 +17,52 @@ import { ChangePhoto } from '@/assets/icons'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 
+// type CardForm = {
+//   question: string
+//   imageQuestion?: FileList
+//   answer: string
+//   imageAnswer?: FileList
+// }
+// type FormImage = 'imageQuestion' | 'imageAnswer'
+//
+// type ImageAdderProps = {
+//   setValue: UseFormSetValue<CardForm>
+//   register: UseFormRegister<CardForm>
+//   cover: string
+//   setCover: Dispatch<SetStateAction<string>>
+//   errorMessage?: string
+//   clearErrors?: UseFormClearErrors<CardForm>
+//   fieldName: FormImage
+// } & ComponentPropsWithoutRef<'input'>
+
+type PackForm = {
+  namePack: string
+  privatePack?: boolean
+  imageCover?: FileList
+}
+type FormImage = 'imageCover'
+
 type ImageAdderProps = {
-  setValue: UseFormSetValue<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
-  register: UseFormRegister<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
+  setValue: UseFormSetValue<PackForm>
+  register: UseFormRegister<PackForm>
   cover: string
   setCover: Dispatch<SetStateAction<string>>
   errorMessage?: string
-  clearErrors?: UseFormClearErrors<{
-    namePack: string
-    privatePack?: boolean
-    imageCover?: FileList
-  }>
+  clearErrors?: UseFormClearErrors<PackForm>
+  fieldName: FormImage
 } & ComponentPropsWithoutRef<'input'>
 
+// type ImageAdderProps<T extends FieldValues> = {
+//   setValue: UseFormSetValue<T>
+//   register: UseFormRegister<T>
+//   cover: string
+//   setCover: Dispatch<SetStateAction<string>>
+//   errorMessage?: string
+//   clearErrors?: UseFormClearErrors<T>
+//   fieldName: string
+// } & ComponentPropsWithoutRef<'input'>
+
+// export const ImageUploader = <T extends FieldValues>({
 export const ImageUploader = ({
   register,
   setValue,
@@ -37,27 +70,29 @@ export const ImageUploader = ({
   setCover,
   errorMessage,
   clearErrors,
+  fieldName,
 }: ImageAdderProps) => {
+  // }: ImageAdderProps<T>) => {
   const hiddenInputRef = useRef<HTMLInputElement | null>(null)
 
   const [key, setKey] = useState<number>(0)
 
   useEffect(() => {
     toast.error(errorMessage)
-    clearErrors && clearErrors('imageCover')
+    clearErrors && clearErrors(fieldName)
   }, [errorMessage])
   const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
 
     if (file) {
       setCover(URL.createObjectURL(file))
-      setValue('imageCover', event.target.files || undefined)
+      setValue(fieldName, event.target.files || undefined)
     }
   }
 
   const handleDeletedFile = () => {
     setCover('')
-    setValue('imageCover', undefined)
+    fieldName && setValue(fieldName, undefined)
     setKey(key => key + 1) //To force re-rendering of the file input element at each call
   }
 
@@ -70,7 +105,7 @@ export const ImageUploader = ({
       <input
         key={key}
         ref={e => {
-          register('imageCover')
+          register(fieldName)
           hiddenInputRef.current = e
         }}
         type="file"
@@ -107,3 +142,186 @@ export const ImageUploader = ({
     </div>
   )
 }
+
+///!!!!!!!!!!!!!!!
+// type ImageAdderProps = {
+//   setValue: UseFormSetValue<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
+//   register: UseFormRegister<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
+//   cover: string
+//   setCover: Dispatch<SetStateAction<string>>
+//   errorMessage?: string
+//   clearErrors?: UseFormClearErrors<{
+//     namePack: string
+//     privatePack?: boolean
+//     imageCover?: FileList
+//   }>
+// } & ComponentPropsWithoutRef<'input'>
+//
+// export const ImageUploader = ({
+//   register,
+//   setValue,
+//   cover,
+//   setCover,
+//   errorMessage,
+//   clearErrors,
+// }: ImageAdderProps) => {
+//   const hiddenInputRef = useRef<HTMLInputElement | null>(null)
+//
+//   const [key, setKey] = useState<number>(0)
+//
+//   useEffect(() => {
+//     toast.error(errorMessage)
+//     clearErrors && clearErrors('imageCover')
+//   }, [errorMessage])
+//   const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
+//     const file = event.target.files?.[0]
+//
+//     if (file) {
+//       setCover(URL.createObjectURL(file))
+//       setValue('imageCover', event.target.files || undefined)
+//     }
+//   }
+//
+//   const handleDeletedFile = () => {
+//     setCover('')
+//     setValue('imageCover', undefined)
+//     setKey(key => key + 1) //To force re-rendering of the file input element at each call
+//   }
+//
+//   const onUpload = () => {
+//     hiddenInputRef.current?.click()
+//   }
+//
+//   return (
+//     <div className={s.modalAddWrapper}>
+//       <input
+//         key={key}
+//         ref={e => {
+//           register('imageCover')
+//           hiddenInputRef.current = e
+//         }}
+//         type="file"
+//         accept="image/*"
+//         className={s.modalAddInputCover}
+//         onChange={handleUploadedFile}
+//       />
+//       <Button
+//         as={'button'}
+//         type={'button'}
+//         variant="secondary"
+//         className={s.modalAddButtonCover}
+//         onClick={onUpload}
+//       >
+//         {cover ? (
+//           <>
+//             <img className={s.modalAddImageCover} src={cover} alt="Not Image" />
+//             <Typography variant={'subtitle2'} className={s.textCoverPreview}>
+//               This preview in cover, click to change.
+//             </Typography>
+//           </>
+//         ) : (
+//           <>
+//             <ChangePhoto onClick={() => {}} />
+//             Click to add a cover, max size 1MB.
+//           </>
+//         )}
+//       </Button>
+//       {cover && (
+//         <Typography variant={'caption'} className={s.deleteCover} onClick={handleDeletedFile}>
+//           ❌
+//         </Typography>
+//       )}
+//     </div>
+//   )
+// }
+///!!!!!!!!!!!!!!!!!!!!
+// type ImageAdderProps = {
+//   setValue: UseFormSetValue<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
+//   register: UseFormRegister<{ namePack: string; privatePack?: boolean; imageCover?: FileList }>
+//   cover: string
+//   setCover: Dispatch<SetStateAction<string>>
+//   errorMessage?: string
+//   clearErrors?: UseFormClearErrors<{
+//     namePack: string
+//     privatePack?: boolean
+//     imageCover?: FileList
+//   }>
+// } & ComponentPropsWithoutRef<'input'>
+//
+// export const ImageUploader = ({
+//   register,
+//   setValue,
+//   cover,
+//   setCover,
+//   errorMessage,
+//   clearErrors,
+// }: ImageAdderProps) => {
+//   const hiddenInputRef = useRef<HTMLInputElement | null>(null)
+//
+//   const [key, setKey] = useState<number>(0)
+//
+//   useEffect(() => {
+//     toast.error(errorMessage)
+//     clearErrors && clearErrors('imageCover')
+//   }, [errorMessage])
+//   const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
+//     const file = event.target.files?.[0]
+//
+//     if (file) {
+//       setCover(URL.createObjectURL(file))
+//       setValue('imageCover', event.target.files || undefined)
+//     }
+//   }
+//
+//   const handleDeletedFile = () => {
+//     setCover('')
+//     setValue('imageCover', undefined)
+//     setKey(key => key + 1) //To force re-rendering of the file input element at each call
+//   }
+//
+//   const onUpload = () => {
+//     hiddenInputRef.current?.click()
+//   }
+//
+//   return (
+//     <div className={s.modalAddWrapper}>
+//       <input
+//         key={key}
+//         ref={e => {
+//           register('imageCover')
+//           hiddenInputRef.current = e
+//         }}
+//         type="file"
+//         accept="image/*"
+//         className={s.modalAddInputCover}
+//         onChange={handleUploadedFile}
+//       />
+//       <Button
+//         as={'button'}
+//         type={'button'}
+//         variant="secondary"
+//         className={s.modalAddButtonCover}
+//         onClick={onUpload}
+//       >
+//         {cover ? (
+//           <>
+//             <img className={s.modalAddImageCover} src={cover} alt="Not Image" />
+//             <Typography variant={'subtitle2'} className={s.textCoverPreview}>
+//               This preview in cover, click to change.
+//             </Typography>
+//           </>
+//         ) : (
+//           <>
+//             <ChangePhoto onClick={() => {}} />
+//             Click to add a cover, max size 1MB.
+//           </>
+//         )}
+//       </Button>
+//       {cover && (
+//         <Typography variant={'caption'} className={s.deleteCover} onClick={handleDeletedFile}>
+//           ❌
+//         </Typography>
+//       )}
+//     </div>
+//   )
+// }
