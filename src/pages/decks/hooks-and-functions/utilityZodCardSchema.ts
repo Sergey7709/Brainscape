f6def1b12e9667f7cfb517b1927a82482d851886
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-20 11:51:02 +0300

    Add utilityZodCardSchema

---
+++ src/pages/decks/hooks-and-functions/utilityZodCardSchema.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod'
+
+import { FILE_SIZE_LIMIT } from '@/pages/decks/constantsDeck.ts'
+
+export const utilityZodCardSchema = () => {
+  const fileSchema = z
+    .any()
+    .refine(fileList => fileList[0]?.size <= FILE_SIZE_LIMIT, `Max file size is 1MB.`)
+
+  const addNewCardSchema = z.object({
+    question: z
+      .string({
+        required_error: 'Question is required',
+      })
+      .trim()
+      .nonempty('Question is required')
+      .min(3, 'Question must be at least 3 characters')
+      .max(30),
+    answer: z
+      .string({
+        required_error: 'Answer is required',
+      })
+      .trim()
+      .nonempty('Answer is required')
+      .min(3, 'Answer must be at least 3 characters')
+      .max(30),
+
+    imageQuestion: fileSchema.optional(),
+    imageAnswer: fileSchema.optional(),
+  })
+
+  type NewCardSchema = z.infer<typeof addNewCardSchema>
+
+  const initialValues: NewCardSchema = {
+    question: '',
+    imageQuestion: undefined,
+    answer: '',
+    imageAnswer: undefined,
+  }
+
+  return { initialValues, addNewCardSchema }
+}
