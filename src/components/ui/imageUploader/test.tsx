e3b54c85fa3ebc93b0979e08b4b4ef0a80a50d02
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-22 14:42:37 +0300

    Delete comments

--- src/components/ui/imageUploader/test.tsx
+++ src/components/ui/imageUploader/test.tsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-
 import { ChangeEvent, useRef, useState } from 'react'
 
 import s from './imageUploader.module.scss'
@@ -8,23 +6,19 @@ import { ChangePhoto } from '@/assets/icons'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 
-// import { FieldName } from 'react-hook-form'
-
 interface A {
-  // value: FileList | undefined
   value: FileList | undefined
-  // onChange: (e: FileList) => void
   onChange: (e: FileList | undefined) => void
-  // resetField: () => void
   resetField: (name: string) => void
-  nameFieldCover: string ///!!!!
-  initialCover: string ///!!!!
+  nameFieldCover: string
+  initialCover: string
 }
 
 export const Test = ({ value, onChange, resetField, initialCover, nameFieldCover }: A) => {
   const hiddenInputRef = useRef<HTMLInputElement | null>(null)
-  // const [cover, setCover] = useState<string | undefined>(value?.fileName)
+
   const initialValue = (value && URL.createObjectURL(value?.[0])) ?? initialCover
+
   const [cover, setCover] = useState<string | undefined>(initialValue)
 
   const handleUploadedFile = (e: ChangeEvent<HTMLInputElement>) => {
@@ -45,15 +39,9 @@ export const Test = ({ value, onChange, resetField, initialCover, nameFieldCover
     setCover('')
   }
 
-  console.log(value, initialCover)
-
   return (
     <>
       <div className={s.modalAddWrapper}>
-        {/*<img*/}
-        {/*  className={s.modalAddImageCover}*/}
-        {/*  src={value ? URL.createObjectURL(value?.[0]) : undefined}*/}
-        {/*/>*/}
         <input
           ref={hiddenInputRef}
           type={'file'}
@@ -61,7 +49,6 @@ export const Test = ({ value, onChange, resetField, initialCover, nameFieldCover
           className={s.modalAddInputCover}
           onChange={handleUploadedFile}
         />
-        {/*<button onClick={onUpload}> Click to add a cover, max size 1MB.</button>*/}
         <Button
           as={'button'}
           type={'button'}
@@ -73,7 +60,6 @@ export const Test = ({ value, onChange, resetField, initialCover, nameFieldCover
             <>
               <img
                 className={s.modalAddImageCover}
-                // src={value ? URL.createObjectURL(value?.[0]) : undefined}
                 src={value ? URL.createObjectURL(value?.[0]) : initialCover}
                 alt="Not Image"
               />
