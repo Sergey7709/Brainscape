36b00d92e035258834d5d656f3f4498c4b6a83aa
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-27 21:03:31 +0300

    Add destructurization for isLoading

--- src/pages/decks/hooks-and-functions/useAddNewCard.ts
+++ src/pages/decks/hooks-and-functions/useAddNewCard.ts
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify'
 import { useCreateCardMutation } from '@/service'
 
 export const useAddNewCard = () => {
-  const [handlerAddNewCardSubmit, isLoading] = useCreateCardMutation()
+  const [handlerAddNewCardSubmit, { isLoading }] = useCreateCardMutation()
 
   const utilityAddNewCard = (deckId: string, body: FormData) => {
     handlerAddNewCardSubmit({ deckId, body })
