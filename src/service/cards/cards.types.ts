b579f572f0ebced27b366b7a2fdd12eb446fc29c
Sergey Koshelev <shivacharya2019@gmail.com>
2023-12-29 16:56:16 +0300

    Add CardTypeFormData

--- src/service/cards/cards.types.ts
+++ src/service/cards/cards.types.ts
@@ -1,6 +1,29 @@
-export type CardType = {
+// export type CardType<T extends 'req' | 'res' = 'res'> = T extends 'req'
+//   ? {
+//       id: string
+//       body: FormData
+//     }
+//   : CardProps
+
+export type CardTypeFormData = {
   id: string
   body: FormData
+}
+
+export type CardType = {
+  id: string
+  deckId: string
+  userId: string
+  question: string
+  answer: string
+  shots: number
+  answerImg: string
+  questionImg: string
+  questionVideo: string
+  answerVideo: string
+  rating: number
+  created: string
+  updated: string
 }
 export type GetCardParams = {
   id: string
