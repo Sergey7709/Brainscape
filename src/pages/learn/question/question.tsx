4d17da740cc72e1164a1e17d7a63f2d4b86c7a31
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-18 11:30:00 +0300

    Delete props: handlerLearn, isLoading, isFetching, add prop openAnswer,  edit conditional render in JSX

--- src/pages/learn/question/question.tsx
+++ src/pages/learn/question/question.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
 import { Typography } from '@/components/ui/typography'
 import s from '@/pages/learn/learn.module.scss'
 
@@ -8,8 +9,7 @@ type QuestionProps = {
   question: string
   shots: number
   handlerLearn: (value: boolean) => void
-  isLoading: boolean
-  isFetching: boolean
+  openAnswer: boolean
 }
 export const Question = ({
   nameCard,
@@ -17,26 +17,23 @@ export const Question = ({
   question,
   shots,
   handlerLearn,
-  isLoading,
-  isFetching,
+  openAnswer,
 }: QuestionProps) => {
   return (
     <>
-      {(isLoading || isFetching) && !imageQuestion && nameCard ? null : (
-        <>
-          <Typography className={s.learnNamePack}>{`Learn "${nameCard}"`}</Typography>
-          <div className={s.learnWrapper}>
-            {imageQuestion && <img className={s.learnImg} src={imageQuestion} alt={'Not Image'} />}
-            <Typography className={s.learnText}>{`Question: ${question}`}</Typography>
-            <Typography className={s.learnCount}>
-              {`Number of attempts to answer the question: ${shots}`}
-            </Typography>
-          </div>
-          <Button onClick={() => handlerLearn(true)} fullWidth>
-            <Typography className={s.learnButtonText}>Show Answer</Typography>
-          </Button>
-        </>
-      )}
+      <Card className={openAnswer ? s.learnTransitionRotate : s.learnCards}>
+        <Typography className={s.learnNamePack}>{`Learn "${nameCard}"`}</Typography>
+        <div className={s.learnWrapper}>
+          {imageQuestion && <img className={s.learnImg} src={imageQuestion} alt={'Not Image'} />}
+          <Typography className={s.learnText}>{`Question: ${question}`}</Typography>
+          <Typography className={s.learnCount}>
+            {`Number of attempts to answer the question: ${shots}`}
+          </Typography>
+        </div>
+        <Button onClick={() => handlerLearn(true)} fullWidth>
+          <Typography className={s.learnButtonText}>Show Answer</Typography>
+        </Button>
+      </Card>
     </>
   )
 }
