cfa81cf722d0a204d82ae0778ed074e44543c53d
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-19 18:05:20 +0300

    Delete comments

--- src/pages/decks/hooks-and-functions/useEditPack.ts
+++ src/pages/decks/hooks-and-functions/useEditPack.ts
@@ -8,11 +8,6 @@ export const useEditPack = () => {
   const utilityEditPack = (id: string, body: FormData) => {
     handlerEditPackSubmit({ id, body })
       .unwrap()
-      // .then(response => {
-      //   if (response && response.id) {
-      //     toast.success('A new package has been added!')
-      //   }
-      // })
       .catch(error => {
         toast.error('Failed edit pack!:', error)
       })
