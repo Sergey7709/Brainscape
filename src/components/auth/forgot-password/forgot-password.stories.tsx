f2ec3e1dc93ef1cadcc069f79c9438a36fb67a45
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-19 14:55:01 +0300

    Edit component, add render.

--- src/components/auth/forgot-password/forgot-password.stories.tsx
+++ src/components/auth/forgot-password/forgot-password.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import s from './forgot-password.module.scss'
 import { ForgotPassword } from './forgot-password.tsx'
+
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
 
 const meta = {
   title: 'Auth/ForgotPassword',
@@ -11,4 +16,28 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  render: () => (
+    <>
+      <Card className={s.card}>
+        <Typography variant="large" as={'h1'} className={s.title}>
+          Forgot your password?
+        </Typography>
+        <form className={s.form}>
+          <Typography variant="body2" className={s.instructions}>
+            Enter your email address and we will send you further instructions
+          </Typography>
+          <Button className={s.buttonSend} fullWidth type={'button'}>
+            Send Instructions
+          </Button>
+        </form>
+        <Typography variant="body2" className={s.caption}>
+          Did you remember your password?
+        </Typography>
+        <Typography variant="link1" as={'a'} className={s.loginLink}>
+          Try logging in
+        </Typography>
+      </Card>
+    </>
+  ),
+}
