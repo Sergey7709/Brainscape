c6b6ad797a38003df8581b507e2320cef172f179
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-21 13:42:59 +0300

    Delete comments

--- src/pages/pack/hooks-and-function/useEditCard.ts
+++ src/pages/pack/hooks-and-function/useEditCard.ts
@@ -8,11 +8,6 @@ export const useEditCard = () => {
   const utilityEditCard = (id: string, body: FormData) => {
     handlerEditCardSubmit({ id, body })
       .unwrap()
-      // .then(response => {
-      //   if (response && response.id) {
-      //     toast.success('A new package has been added!')
-      //   }
-      // })
       .catch(error => {
         toast.error('Failed edit card!:', error)
       })
