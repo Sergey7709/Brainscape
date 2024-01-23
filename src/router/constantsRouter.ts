7c7bd6881b40e8a88ab9a912a750046bd25bafe9
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-23 14:52:37 +0300

    Create and add export constantsRouter.ts

---
+++ src/router/constantsRouter.ts
@@ -0,0 +1,21 @@
+export const greetingNonAuthorizedPath = '/greeting'
+
+export const loginPath = '/login'
+
+export const registrationPath = '/sign-up'
+
+export const verifyEmailPath = '/check-email'
+
+export const createNewPasswordPath = '/confirm-email/:token'
+
+export const passwordRecoveryPath = '/forgot-password'
+
+export const profilePath = '/profile'
+
+export const decksPath = '/deck'
+
+export const chartsPath = '/charts'
+
+export const packPath = '/pack/:id'
+
+export const learnPath = '/learn/:id'
