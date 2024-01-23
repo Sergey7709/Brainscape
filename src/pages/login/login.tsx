ccaaacd515b1d09026e00cf5b6c95342963727a8
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-23 16:04:20 +0300

    Replace path name for rout Layout and GreetingAuthorized on rootElementPath variable

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom'
 import s from './login.module.scss'
 
 import { SignIn } from '@/components/auth/sign-in'
+import { rootElementPath } from '@/router'
 import { useGetAuthUserMeDataQuery, useSignInUserMutation } from '@/service'
 
 export const Login = () => {
@@ -12,7 +13,7 @@ export const Login = () => {
 
   return (
     <div className={s.login}>
-      {isAuthenticated && <Navigate to={'/'} replace={true} />}
+      {isAuthenticated && <Navigate to={rootElementPath} replace={true} />}
       {isError && <SignIn onHandleSubmit={signInUser} />}
     </div>
   )
