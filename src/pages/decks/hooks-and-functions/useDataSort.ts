afe54732f6ee8a0af501ab0f2811b78888c8ce34
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-25 12:53:09 +0300

    Replace string 'orderBy' on variable orderBy

--- src/pages/decks/hooks-and-functions/useDataSort.ts
+++ src/pages/decks/hooks-and-functions/useDataSort.ts
@@ -2,10 +2,11 @@ import { useSearchParams } from 'react-router-dom'
 
 import { Sort } from '@/components/ui/tables'
 import { orderByValue } from '@/utils'
+import { orderBy } from '@/utils/constants/constantsForSearchParams.ts'
 
 export const useDataSort = () => {
   const [searchParams] = useSearchParams()
-  const sortString = searchParams.get('orderBy') ?? orderByValue
+  const sortString = searchParams.get(orderBy) ?? orderByValue
 
   const [key, direction] = sortString.split('-')
   const sort = { key, direction } as Sort
--- src/pages/pack/tablePack/tablePack.tsx
+++ src/pages/pack/tablePack/tablePack.tsx
@@ -5,6 +5,7 @@ import s from '@/pages/pack/tablePack/tablePack.module.scss'
 import { GetEntitiesResponse } from '@/service/common/types.ts'
 import { PackCards } from '@/service/decks/decks.types.ts'
 import { useUtilityForSearchParamsEdit } from '@/utils'
+import { orderBy } from '@/utils/constants/constantsForSearchParams.ts'
 
 type TablePackProps = {
   dataCards: GetEntitiesResponse<PackCards>
@@ -16,7 +17,7 @@ export const TablePack = ({ dataCards, sort, mePackCards }: TablePackProps) => {
 
   const handlerSortValuePack = (sort: Sort) => {
     utilityForSearchParamsEdit({
-      param: 'orderBy',
+      param: orderBy,
       valueForNewParam:
         sort?.key && sort?.direction !== null ? `${sort?.key}-${sort?.direction}` : [],
     })
