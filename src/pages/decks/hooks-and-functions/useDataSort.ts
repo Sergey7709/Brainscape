87f392b934dc413f460f7c95de8b124b51605d99
Sergey Koshelev <shivacharya2019@gmail.com>
2024-01-25 13:10:42 +0300

    Replace orderBy on variable orderByParams

--- src/pages/decks/hooks-and-functions/useDataSort.ts
+++ src/pages/decks/hooks-and-functions/useDataSort.ts
@@ -2,11 +2,11 @@ import { useSearchParams } from 'react-router-dom'
 
 import { Sort } from '@/components/ui/tables'
 import { orderByValue } from '@/utils'
-import { orderBy } from '@/utils/constants/constantsForSearchParams.ts'
+import { orderByParams } from '@/utils/constants/constantsForSearchParams.ts'
 
 export const useDataSort = () => {
   const [searchParams] = useSearchParams()
-  const sortString = searchParams.get(orderBy) ?? orderByValue
+  const sortString = searchParams.get(orderByParams) ?? orderByValue
 
   const [key, direction] = sortString.split('-')
   const sort = { key, direction } as Sort
